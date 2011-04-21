f5e53c09017ddf1b05e137fe2841839412b790c8
Chad Baker <calbaker@gmail.com>
2011-04-21 10:30:13 -0500

    Not sure what I did here.

--- instance.py
+++ instance.py
@@ -12,7 +12,6 @@ reload(HX)
 
 print "Beginning execution..."
 
-# Instantiation
 HX1 = HX.HX()
 HX1.exh.porous = 'no' 
 HX1.exh.T_inlet = 600.
@@ -24,14 +23,51 @@ print "Program finished."
 
 print "Plotting..."
 
-mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.exh.T_nodes,
-         label='Exhaust')
-mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.cool.T_nodes,
+x = sp.arange(HX1.nodes) * HX1.node_length * 100
+
+mpl.figure()
+mpl.plot(x, HX1.cool.T_nodes,
          label='Coolant')
-mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.TEM.T_hot,
-         label='TEM Hot Side')
-mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.TEM.T_cool,
+mpl.plot(x, HX1.TEM.T_cool,
          label='TEM Cold Side')
+mpl.plot(x, HX1.TEM.T_hot,
+         label='TEM Hot Side')
+mpl.plot(x, HX1.exh.T_nodes,
+         label='Exhaust')
+
+mpl.xlabel('Distance Along HX (m)')
+mpl.ylabel('Temperature (K)')
+mpl.title('Temperature v. Distance Along HX')
+mpl.grid()
+mpl.legend()
+
+mpl.figure()
+mpl.plot(x, HX1.cool.T_nodes,
+         label='Coolant')
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
+         HX1.plate.h**-1)**-1 * HX1.A), label='TEM Cold Side') 
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
+         HX1.plate.h**-1 + HX1.TEM.h**-1)**-1 * HX1.A), label='TEM' +
+         ' Hot Side') 
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
+         HX1.plate.h**-1 + HX1.TEM.h**-1 + HX1.plate.h**-1 +
+         HX1.exh.h**-1)**-1 * HX1.A), label='Exhaust') 
+
+mpl.xlabel('Distance Along HX (m)')
+mpl.ylabel('Temperature (K)')
+mpl.title('Temperature v. Distance Along HX')
+mpl.grid()
+mpl.legend()
+
+mpl.figure()
+mpl.plot(x, HX1.cool.T_nodes,label='Coolant')
+
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes * (HX1.cool.R_thermal + HX1.plate.R_thermal) / HX1.A, label='TEM Cold Side')  
+
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes * (HX1.cool.R_thermal + HX1.plate.R_thermal + HX1.TEM.R_thermal) / HX1.A, label='TEM Hot Side') 
+
+mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes * (HX1.cool.R_thermal + HX1.plate.R_thermal + HX1.TEM.R_thermal + HX1.plate.R_thermal + HX1.exh.R_thermal) / HX1.A, label='Exhaust') 
+
 mpl.xlabel('Distance Along HX (m)')
 mpl.ylabel('Temperature (K)')
 mpl.title('Temperature v. Distance Along HX')
@@ -39,3 +75,4 @@ mpl.grid()
 mpl.legend()
 
 mpl.show()
+
