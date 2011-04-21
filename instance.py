d7ce4dad17b95068c13c634851ca27d15e4cf3d5
Chad Baker <calbaker@gmail.com>
2011-04-21 09:34:15 -0500

    Added reload(HX) statement so that changes in HX.py are reflected when running instance.py.

--- instance.py
+++ instance.py
@@ -3,14 +3,12 @@
 
 # Distribution Modules
 import scipy as sp
-import numpy as np
 import matplotlib.pyplot as mpl
 
 # User Defined Modules
 # In this directory
 import HX
-# In python directory
-import properties as prop
+reload(HX)
 
 print "Beginning execution..."
 
@@ -23,3 +21,21 @@ HX1.cool.T_inlet = 300.
 HX1.solve_HX()
 
 print "Program finished."
+
+print "Plotting..."
+
+mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.exh.T_nodes,
+         label='Exhaust')
+mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.cool.T_nodes,
+         label='Coolant')
+mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.TEM.T_hot,
+         label='TEM Hot Side')
+mpl.plot(sp.arange(HX1.nodes) * HX1.node_length * 100, HX1.TEM.T_cool,
+         label='TEM Cold Side')
+mpl.xlabel('Distance Along HX (m)')
+mpl.ylabel('Temperature (K)')
+mpl.title('Temperature v. Distance Along HX')
+mpl.grid()
+mpl.legend()
+
+mpl.show()
