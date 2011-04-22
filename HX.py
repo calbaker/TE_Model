199e04d13a93ebb69192f988746599d381a9ee62
Chad Baker <calbaker@gmail.com>
2011-04-22 12:19:21 -0500

    exhaust, coolant, and functions have been moved outside of HX.py as modules.

--- HX.py
+++ HX.py
@@ -10,32 +10,10 @@ import os
 # In this directory
 import engine
 import TEM
-reload(TEM)
-
-# In python directory
-import properties as prop
-reload(prop) # uncomment this when making changes in properties
+import exhaust
+import coolant
 
 # definitions of classes for mediums in which heat transfer can occur
-
-def set_flow_geometry(self):
- self.perimeter = 2.*(self.height+HX.width) # wetted perimeter (m) of
-                                        # exhaust flow
- self.area = self.height * HX.width # cross-section area (m^2) of
-                                    # exhaust flow
- self.D = 4.*self.area / self.perimeter # coolant hydraulic diameter (m)
-
-def set_Re_dependents(self):
- self.set_Re()
- if self.Re_D > 2300.: # Do these correlations hold for any tube geometry?
-  self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
-  # flow from Bejan Eq. 8.13
-  self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
-  # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
- else:
-  self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
-   # parallel plates with constant T
-  self.f = 24./self.Re_D
 
 class plate_wall():
  k = 0.2 # thermal conductivity (kW/m-K) of Aluminum HX plate
@@ -53,121 +31,21 @@ class TEdummy():
   self.h = 0.6 # guessed effective coefficient of convection (kW/m^2-K) for
          # TE device         
 
-class exhaust(prop.ideal_gas):
- porous = 'no' # is there porous media?
- enhancement = 'none' # is there any means of enhancement? (i.e. fins,
-                      # corrugate metal, etc.)
-
- T_ref = 300 # reference temperature (K) for availability calculation
-
- height = 1.25e-2 # height (m) of exhaust duct
-
- ducts = 1 # number of exhaust ducts
- porosity = 0.92 # volume of void per total volume
- k_matrix = 5.8e-3 # thermal conductivity(kW/m-K) of metal foam +
-  # air
- PPI = 10 # pores per inch of porous media, used in Mancin model
- K = 2.e-7 # permeability (m^2) of porous metal foam, used in Bejan model
-
- set_flow_geometry = set_flow_geometry
- set_Re_dependents = set_Re_dependents
-
- def set_flow(self):
-  self.T = 0.5 * (self.T_in + self.T_out) # Temperature (K) used to calculate fluid
-                     # properties.  This is no good if T_out is much
-                     # different from T_in
-  self.set_TempPres_dependents()
-  self.c_p = self.c_p_air
-
-  self.C = self.mdot * self.c_p # heat capacity of
-  # flow (kW/K)
-  self.Vdot = self.mdot / self.rho # volume flow rate (m^3/s) of exhaust
-  print "Exhaust Vdot =",self.Vdot
-  self.velocity = self.Vdot / (HX.width * self.height)
-  print "Exhaust velocity =",self.velocity
-
-  if self.porous == 'Bejan':
-   self.Nu_D = 6. # Nu for porous media parallel plates with const
-                  # heat flux.  Bejan Eq. 12.77 
-   self.Re_K = self.velocity * self.K**0.5 / self.nu # Re
-    # based on permeability from
-    # Bejan Eq. 12.11    
-   self.f = 1. / self.Re_K + 0.55 # Darcy Law, Bejan
-    # Eq. 12.14.  It turns out
-    # that f is pretty close to
-    # 0.55
-   self.k = self.k_matrix
-   self.deltaP = (self.f * self.perimeter * HX.length /
-    self.area * (0.5*self.rho * self.velocity**2)*1.e-3) # pressure drop (kPa)
-
-
-  elif self.porous == 'Mancin':
-   self.k = self.k_matrix
-   self.Nu_D = 6. # Nu for porous media parallel plates with
-                          # q''= const.  Bejan Eq. 12.77
-   self.G = self.rho * self.velocity # Mass velocity from
-                                        # Mancin et al.
-   self.D_pore = 0.0122 * self.PPI**(-0.849) # hydraulic
-                                        # diameter (m?) of porous
-                                        # media based on Mancin et
-                                        # al.  
-   self.Re_K = ( self.D_pore * self.G / (self.mu * self.porosity) )
-   # Re of porous media from Mancin et al.
-   self.F = ( (1.765 * self.Re_K**(-0.1014) * self.porosity**2 /
-   self.PPI**(0.6)) ) # friction factorb from Mancin et al. 
-   self.deltaP = (HX.length * 2. * self.F * self.G**2 /
-   (self.D_pore * self.rho)) # pressure drop from Mancin et al.
-
-  elif self.enhancement == 'none':
-   self.k = self.k_air
-   self.set_Re_dependents()
-   self.deltaP = (self.f * self.perimeter * HX.length /
-    self.area * (0.5*self.rho * self.velocity**2)*1.e-3) # pressure drop (kPa)
-
-  elif self.enhancement == 'corrugated':
-   self.k = self.k_air
-   self.set_Re_dependents()
-   self.deltaP = (self.f * self.perimeter * HX.length /
-    self.area * (0.5*self.rho * self.velocity**2)*1.e-3) # pressure drop (kPa)
-   
-  self.h = self.Nu_D * self.k / self.D # coefficient of convection (kW/m^2-K)
-  self.Wdot_pumping = self.Vdot * self.deltaP # pumping power (kW)
-    
-class coolant(prop.flow):
- height = 0.25e-2 # height (m) of coolant duct
- mdot = 0.5 # mass flow rate (kg/s) of coolant
- ducts = 2 # number of coolant ducts per hot duct
- geometry = 'parallel plates'
- c_p = 4.179 # Specific heat (kJ/kg*K) of water at 325K 
- mu = 5.3e-4 # viscosity of water at 325K (Pa*s), WolframAlpha
- k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K) 
- # through cooling duct
- Pr = (7.01 + 5.43)/2 # Prandtl # of water from Engineering
-  # Toolbox
- rho = 1000. # density (kg/m**3) of water
- k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K)
-
- set_flow_geometry = set_flow_geometry
- set_Re_dependents = set_Re_dependents
-
- def set_flow(self):
-  self.T = 0.5 * (self.T_in + self.T_out) # Temperature (K) used to calculate fluid
-                     # properties.  This is no good if T_out is much
-                     # different from T_in
-  self.C = self.mdot * self.c_p # heat capacity of flow (kW/K)
-  self.Vdot = self.mdot / self.rho # volume flow rate (m^3/s) of exhaust
-  self.velocity = self.Vdot / (HX.width * self.height * self.ducts)
-  self.nu = self.mu/self.rho
-  self.set_Re_dependents()
-  self.h = self.Nu_D * self.k / self.D # coefficient of convection (kW/m^2-K)
-  self.deltaP =  ( self.f * self.perimeter * HX.length / self.area *
-  (0.5*self.rho * self.velocity**2)*1.e-3 ) # pressure drop (kPa) 
-  self.Wdot_pumping = self.Vdot * self.deltaP # pumping power (kW)
-
 class HX:
+ def __init__(self):
+  # Geometry and constants 
+  self.width = 15e-2 # width (cm*10**-2) of HX duct. This model treats
+   # duct as parallel plates for simpler modeling.
+  self.length = 1. # length (m) of HX duct
+  self.nodes = 20 # number of nodes for numerical heat transfer model
+  self.node_length = self.length / self.nodes # length (m) of each node
+  self.x_dim = sp.arange(self.node_length/2, self.length +
+  self.node_length/2, self.node_length)  
+  # x coordinate (m)
+
  # initilization of class instances
- cool = coolant()
- exh = exhaust()
+ cool = coolant.coolant()
+ exh = exhaust.exhaust()
  TEM = TEM.TEModule()
  plate = plate_wall()
  Cummins = engine.engine()
@@ -176,14 +54,6 @@ class HX:
  Cummins.set_mdot_charge() # mass flow rate of exhaust
  exh.mdot = Cummins.mdot_charge
 
- # Geometry and constants 
- width = 15e-2 # width (cm*10**-2) of HX duct. This model treats
-  # duct as parallel plates for simpler modeling.
- length = 1. # length (m) of HX duct
- nodes = 20 # number of nodes for numerical heat transfer model
- node_length = length / nodes # length (m) of each node
- x_dim = sp.arange(node_length/2, length + node_length/2, node_length)
- # x coordinate (m)
 
  def solve_node(self):
   self.exh.T_out = self.exh.T_in - 5 # Guess at exhaust node out temperature (K)
@@ -230,7 +100,8 @@ class HX:
   # temperature (K) at coolant outlet
 
  def solve_HX(self):
-  self.exh.set_flow_geometry()
+  self.exh.set_flow_geometry() # this should be moved to the
+                               # corresponding module
   self.cool.set_flow_geometry()
 
   self.A = self.node_length*self.width*self.cool.ducts # area (m^2)
@@ -244,24 +115,25 @@ class HX:
   self.cool.T_in = self.cool.T_inlet
 
   # initializing arrays for tracking variables at nodes
-  self.Qdot_nodes = sp.zeros(self.nodes) # initialize array for storing
+  zeros = sp.zeros(self.nodes)
+  self.Qdot_nodes = zeros # initialize array for storing
                                     # heat transfer (kW) in each node 
-  self.effectiveness_nodes = sp.zeros(self.nodes) # initialize array for storing
+  self.effectiveness_nodes = zeros # initialize array for storing
                                     # heat transfer (kW) in each node 
-  self.exh.T_nodes = sp.zeros(self.nodes) # initializing array for storing
+  self.exh.T_nodes = zeros # initializing array for storing
                                      # temperature (K) in each node 
-  self.exh.h_nodes = sp.zeros(self.nodes)
-  self.cool.T_nodes = sp.zeros(self.nodes) # initializing array for storing
+  self.exh.h_nodes = zeros
+  self.cool.T_nodes = zeros # initializing array for storing
                                      # temperature (K) in each node 
-  self.cool.h_nodes = sp.zeros(self.nodes) 
-  self.U_nodes = sp.zeros(self.nodes) 
-  self.TEM.T_cool = sp.zeros(self.nodes) # initializing array for storing
+  self.cool.h_nodes = zeros 
+  self.U_nodes = zeros 
+  self.TEM.T_cool = zeros # initializing array for storing
                                      # temperature (K) in each node 
-  self.TEM.T_hot = sp.zeros(self.nodes) # initializing array for storing
+  self.TEM.T_hot = zeros # initializing array for storing
                                      # temperature (K) in each node 
-  self.TEM.I_nodes = sp.zeros(self.nodes)
-  self.TEM.V_nodes = sp.zeros(self.nodes)
-  self.TEM.power_nodes = sp.zeros(self.nodes)
+  self.TEM.I_nodes = zeros
+  self.TEM.V_nodes = zeros
+  self.TEM.power_nodes = zeros
 
   for i in sp.arange(self.nodes):
    print "\nSolving node", i
