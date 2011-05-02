df5b722d1018e40c827d8572ff7aa8e07d8f3a17
Chad Baker <calbaker@mail.utexas.edu>
2011-05-02 10:47:08 -0500

    Made it so that if only inlet or outlet temperature is defined, the other will be set equal for purposes of property calculations.

--- coolant.py
+++ coolant.py
@@ -4,23 +4,40 @@ import properties as prop
 from functions import *
 
 class coolant(prop.flow):
- height = 0.25e-2 # height (m) of coolant duct
- mdot = 0.5 # mass flow rate (kg/s) of coolant
- ducts = 2 # number of coolant ducts per hot duct
- geometry = 'parallel plates'
- c_p = 4.179 # Specific heat (kJ/kg*K) of water at 325K 
- mu = 5.3e-4 # viscosity of water at 325K (Pa*s), WolframAlpha
- k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K) 
- # through cooling duct
- Pr = (7.01 + 5.43)/2 # Prandtl # of water from Engineering
+ def __init__(self):
+  self.height = 0.25e-2 # height (m) of coolant duct
+  self.mdot = 0.5 # mass flow rate (kg/s) of coolant
+  self.ducts = 2 # number of coolant ducts per hot duct
+  self.geometry = 'parallel plates'
+  self.c_p = 4.179 # Specific heat (kJ/kg*K) of water at 325K 
+  self.mu = 5.3e-4 # viscosity of water at 325K (Pa*s), WolframAlpha
+  self.k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K) 
+  # through cooling duct
+  self.Pr = (7.01 + 5.43)/2 # Prandtl # of water from Engineering
   # Toolbox
- rho = 1000. # density (kg/m**3) of water
- k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K)
-
+  self.rho = 1000. # density (kg/m**3) of water
+  self.k = 0.646e-3 # thermal conductivity of water at 325K (kW/m*K)
+  
  set_flow_geometry = set_flow_geometry
  set_Re_dependents = set_Re_dependents
-
+  
  def set_flow(self,length):
+  # If T_out is not defined, set it equal to T_in since it really
+  # doesn't matter for calculations. Also, vice versa.
+  try:
+   self.T_out
+  except AttributeError:
+   self.T_out = None
+  try:
+   self.T_in
+  except AttributeError:
+   self.T_in = None
+  if self.T_out is None:
+   self.T_out = self.T_in
+  elif self.T_in is None:
+   self.T_in = self.T_outlet
+
+  # Resume with other calculations
   self.T = 0.5 * (self.T_in + self.T_out) # Temperature (K) used to calculate fluid
                      # properties.  This is no good if T_out is much
                      # different from T_in
