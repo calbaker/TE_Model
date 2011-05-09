f0a3f78bb82237e9419bceaad57d1c314c5e4685
Chad Baker <calbaker@mail.utexas.edu>
2011-05-09 18:44:36 -0500

    work

--- tem_instance.py
+++ tem_instance.py
@@ -1,8 +1,10 @@
 # distribution modules
 import scipy as sp
+import matplotlib.pyplot as mpl
 
 # local user modules
 import tem
+reload(tem)
 
 leg = tem.Leg()
 leg.material = 'HMS'
@@ -18,15 +20,23 @@ mpl.rcParams['ytick.labelsize'] = FONTSIZE
 mpl.rcParams['lines.linewidth'] = 1.5
 mpl.rcParams['lines.markersize'] = 10
 
-for i in sp.arange(4,sp.size(leg.q_c)):
-    mpl.plot(sp.absolute(leg.q_c[i]), leg.T_h_guess[i], 'o', label=str(i))
-mpl.plot(sp.absolute(leg.q_c), leg.T_h_guess, '-k')    
+q_c = sp.arange(0.1,10.,0.1) * leg.q_c[0]
+T_h = sp.zeros(sp.size(q_c))
+for i in sp.arange(sp.size(q_c)):
+    leg.q[0] = q_c[i]
+    leg.solve_leg_once()
+    T_h[i] = leg.T[-1]
 
-mpl.ylim(500,600)
-mpl.xlabel(r'$q_c$')
-mpl.ylabel(r'$T_h$')
-mpl.legend(loc='right')
-mpl.legend
+dTdq = sp.zeros(sp.size(leg.q_c) - 1)    
+
+for j in sp.arange(sp.size(leg.q_c) - 1):
+    dTdq[j] = (leg.T_h[j + 1] - leg.T_h[j]) / (leg.q_c[j + 1] - leg.q_c[j])
+
+fig2 = mpl.figure()
+mpl.title('TE Hot Side Temp v. Cold Side Heat')
+mpl.plot(q_c*1.e-3, T_h)
+mpl.xlabel(r'Cold Side $q^{\prime\prime}$ $\left[\frac{kW}{m^2 K}\right]$')
+mpl.ylabel('Hot Side Temperature (K)')
 mpl.grid()
 
 mpl.show()
