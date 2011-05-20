16830eb42ccd7cdf6c6ca0bfe82c8ea8ef1115e9
Chad Baker <calbaker@mail.utexas.edu>
2011-05-20 14:37:23 -0500

    works great

--- instance.py
+++ instance.py
@@ -15,22 +15,22 @@ import tem
 
 print "Beginning execution..."
 
-HX1 = hx.HX()
-HX1.type = 'parallel'
-HX1.tem = tem.TECarnot()
-HX1.exh.T_inlet = 800.
-HX1.exh.P = 100.
-HX1.cool.T_inlet = 300.
-#HX1.tem.set_ZT()
-HX1.solve_hx()
+hx_parallel = hx.HX()
+hx_parallel.type = 'parallel'
+hx_parallel.tem = tem.TEModule()
+hx_parallel.exh.T_inlet = 800.
+hx_parallel.exh.P = 100.
+hx_parallel.cool.T_inlet = 300.
+#hx_parallel.tem.set_ZT()
+hx_parallel.solve_hx()
 
-# HX2 = hx.HX()
-# HX2.exh.porous = 'no'
-# HX2.type = 'counter'
-# HX2.exh.T_inlet = 600.
-# HX2.exh.P = 100.
-# HX2.cool.T_outlet = 306.
-# HX2.solve_hx()
+hx_counter = hx.HX()
+hx_counter.exh.porous = 'no'
+hx_counter.type = 'counter'
+hx_counter.exh.T_inlet = 600.
+hx_counter.exh.P = 100.
+hx_counter.cool.T_outlet = 306.
+hx_counter.solve_hx()
 
 print "\nProgram finished."
 print "\nPlotting..."
@@ -45,45 +45,32 @@ mpl.rcParams['ytick.labelsize'] = FONTSIZE
 mpl.rcParams['lines.linewidth'] = 1.5
 
 mpl.figure()
-mpl.plot(HX1.x_dim, HX1.exh.T_nodes, '-r', label='Exhaust')
-mpl.plot(HX1.x_dim, HX1.tem.T_h_nodes, '-g', label='TEM Hot Side')
-mpl.plot(HX1.x_dim, HX1.tem.T_c_nodes, '-k', label='TEM Cold Side')
-mpl.plot(HX1.x_dim, HX1.cool.T_nodes, '-b', label='Coolant')
+mpl.plot(hx_parallel.x_dim, hx_parallel.exh.T_nodes, '-r', label='Exhaust')
+mpl.plot(hx_parallel.x_dim, hx_parallel.tem.T_h_nodes, '-g', label='TEM Hot Side')
+mpl.plot(hx_parallel.x_dim, hx_parallel.tem.T_c_nodes, '-k', label='TEM Cold Side')
+mpl.plot(hx_parallel.x_dim, hx_parallel.cool.T_nodes, '-b', label='Coolant')
 
 mpl.xlabel('Distance Along HX (m)')
 mpl.ylabel('Temperature (K)')
-mpl.title('Temperature v. Distance, '+HX1.type)
+mpl.title('Temperature v. Distance, '+hx_parallel.type)
 mpl.grid()
 mpl.legend(loc='best')
-mpl.savefig('Plots/temp '+HX1.type+'.png')
-mpl.savefig('Plots/temp '+HX1.type+'.pdf')
+mpl.savefig('Plots/temp '+hx_parallel.type+'.png')
+mpl.savefig('Plots/temp '+hx_parallel.type+'.pdf')
 
-# calculates T using resistance network as a check.  
-# mpl.figure()
-# mpl.plot(x, HX1.cool.T_nodes,
-#          label='Coolant')
-# mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
-#          HX1.plate.h**-1)**-1 * HX1.A), label='TEM Cold Side') 
-# mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
-#          HX1.plate.h**-1 + HX1.TEM.h**-1)**-1 * HX1.A), label='TEM' +
-#          ' Hot Side') 
-# mpl.plot(x, HX1.cool.T_nodes + HX1.Qdot_nodes / ((HX1.cool.h**-1 +
-#          HX1.plate.h**-1 + HX1.TEM.h**-1 + HX1.plate.h**-1 +
-#          HX1.exh.h**-1)**-1 * HX1.A), label='Exhaust') 
+mpl.figure()
+mpl.plot(hx_counter.x_dim, hx_counter.exh.T_nodes, '-r', label='Exhaust')
+mpl.plot(hx_counter.x_dim, hx_counter.tem.T_h_nodes, '-g', label='TEM Hot Side')
+mpl.plot(hx_counter.x_dim, hx_counter.tem.T_c_nodes, '-k', label='TEM Cold Side')
+mpl.plot(hx_counter.x_dim, hx_counter.cool.T_nodes, '-b', label='Coolant')
 
-# mpl.xlabel('Distance Along HX (m)')
-# mpl.ylabel('Temperature (K)')
-# mpl.title('Temperature v. Distance Along HX')
-# mpl.grid()
-# mpl.legend()
-# mpl.savefig('Plots/convection temp.png')
-# mpl.savefig('Plots/convection temp.pdf')
+mpl.xlabel('Distance Along HX (m)')
+mpl.ylabel('Temperature (K)')
+mpl.title('Temperature v. Distance, '+hx_counter.type)
+mpl.grid()
+mpl.legend(loc='best')
+mpl.savefig('Plots/temp '+hx_counter.type+'.png')
+mpl.savefig('Plots/temp '+hx_counter.type+'.pdf')
 
 mpl.show()
 
-DUDh_exh = HX1.exh.h**-2 * HX1.U**-2 # derivative of overall heat
-                                     # transfer w.r.t. exhaust heat
-                                     # transfer coefficient  
-DUDh_plate = HX1.plate.h**-2 * HX1.U**-2 # derivative of overall heat
-                                     # transfer w.r.t. plate heat
-                                     # transfer coefficient 
