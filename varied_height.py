54752a2b48a5ffe73ef269bab5e4c71e5e1e2b9e
Chad Baker <calbaker@mail.utexas.edu>
2011-05-20 14:37:58 -0500

    works great

--- varied_height.py
+++ varied_height.py
@@ -15,29 +15,29 @@ import tem
 
 print "Beginning execution..."
 
-HX1 = hx.HX()
-HX1.type = 'parallel'
-HX1.tem = tem.TECarnot()
-HX1.exh.T_inlet = 700.
-HX1.exh.P = 100.
-HX1.cool.T_inlet = 300.
-#HX1.tem.set_ZT()
+hx = hx.HX()
+hx.type = 'parallel'
+hx.tem = tem.TECarnot()
+hx.exh.T_inlet = 700.
+hx.exh.P = 100.
+hx.cool.T_inlet = 300.
+#hx.tem.set_ZT()
 
-HX1.exh.height_array = sp.arange(1., 5., 0.05) * 1.e-2
+hx.exh.height_array = sp.arange(1., 5., 0.05) * 1.e-2
 # array for varied exhaust duct height (m)
-array_size = sp.size(HX1.exh.height_array)
-HX1.power_net_array = sp.zeros(array_size)
-HX1.Wdot_pumping_array = sp.zeros(array_size)
-HX1.Qdot_array = sp.zeros(array_size)
-HX1.tem.power_array = sp.zeros(array_size)
+array_size = sp.size(hx.exh.height_array)
+hx.power_net_array = sp.zeros(array_size)
+hx.Wdot_pumping_array = sp.zeros(array_size)
+hx.Qdot_array = sp.zeros(array_size)
+hx.tem.power_array = sp.zeros(array_size)
 
-for i in sp.arange(sp.size(HX1.exh.height_array)):
-    HX1.exh.height = HX1.exh.height_array[i]
-    HX1.solve_hx()
-    HX1.power_net_array[i] = HX1.power_net
-    HX1.Wdot_pumping_array[i] = HX1.Wdot_pumping
-    HX1.Qdot_array[i] = HX1.Qdot
-    HX1.tem.power_array[i] = HX1.tem.power
+for i in sp.arange(sp.size(hx.exh.height_array)):
+    hx.exh.height = hx.exh.height_array[i]
+    hx.solve_hx()
+    hx.power_net_array[i] = hx.power_net
+    hx.Wdot_pumping_array[i] = hx.Wdot_pumping
+    hx.Qdot_array[i] = hx.Qdot
+    hx.tem.power_array[i] = hx.tem.power
 
 print "\nProgram finished."
 print "\nPlotting..."
@@ -52,13 +52,13 @@ mpl.rcParams['ytick.labelsize'] = FONTSIZE
 mpl.rcParams['lines.linewidth'] = 1.5
 
 mpl.figure()
-mpl.plot(HX1.exh.height_array * 100., HX1.power_net_array,
+mpl.plot(hx.exh.height_array * 100., hx.power_net_array,
          label='Net')  
-mpl.plot(HX1.exh.height_array * 100., HX1.Wdot_pumping_array,
+mpl.plot(hx.exh.height_array * 100., hx.Wdot_pumping_array,
          label='Pumping')
-mpl.plot(HX1.exh.height_array * 100., HX1.tem.power_array,
+mpl.plot(hx.exh.height_array * 100., hx.tem.power_array,
          label='TEM')
-mpl.plot(HX1.exh.height_array * 100., HX1.Qdot_array,
+mpl.plot(hx.exh.height_array * 100., hx.Qdot_array,
          label=r'$\dot{Q}$') 
 mpl.grid()
 mpl.xlabel('Exhaust Duct Height (cm)')
