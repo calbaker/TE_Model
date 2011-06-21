e8332b3907535c1e91524818b67922379d3f4759
Chad Baker <calbaker@gmail.com>
2011-06-21 17:29:32 -0500

    moved bypass variable to exhaust init

--- hx.py
+++ hx.py
@@ -34,22 +34,22 @@ class HX():
         self.x_dim = sp.arange(self.node_length/2, self.length +
         self.node_length/2, self.node_length)  
         # x coordinate (m)
-        self.bypass = 0.80
-        # fraction of exhaust flow bypassing heat exchanger
+        self.initialize_subclasses()
 
-    cool = coolant.Coolant()
-    cool.width = self.width
-    cool.length = self.length
-    exh = exhaust.Exhaust()
-    exh.width = self.width
-    exh.length = self.length
-    tem = tem.TEModule()
-    plate = _PlateWall()
-    cummins = engine.Engine()
+        # initialization of sub classes
+        self.cool = coolant.Coolant()
+        self.cool.width = self.width
+        self.cool.length = self.length
+        self.exh = exhaust.Exhaust()
+        self.exh.width = self.width
+        self.exh.length = self.length
+        self.tem = tem.TEModule()
+        self.plate = _PlateWall()
+        self.cummins = engine.Engine()
 
     def set_mdot_charge(self):
         self.cummins.set_mdot_charge() # mass flow rate (kg/s) of exhaust
-        self.exh.mdot = self.cummins.mdot_charge * (1. - self.bypass) 
+        self.exh.mdot = self.cummins.mdot_charge * (1. - self.exh.bypass) 
 
     def solve_node(self):
         """solves for performance of streamwise slice of HX"""
