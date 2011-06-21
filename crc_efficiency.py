82aad081323455f594cb17c6503f3d4b81c30591
Chad Baker <calbaker@gmail.com>
2011-06-21 13:51:53 -0500

    yep

--- crc_efficiency.py
+++ crc_efficiency.py
@@ -25,7 +25,7 @@ Ptype.area = (0.01)**2 # area (m^2)
 Ptype.T_h_goal = 750.
 Ptype.T_c = 400.
 
-Ntype.current_density = sp.arange(20., 51., 1.) * 100.**2
+Ntype.current_density = sp.arange(15., 51., 1.) * 100.**2
 Ntype.currents = Ntype.current_density * Ntype.area
 Ntype.etas = sp.zeros(sp.size(Ntype.currents))
 for i in sp.arange(sp.size(Ntype.currents)):
@@ -33,7 +33,7 @@ for i in sp.arange(sp.size(Ntype.currents)):
     Ntype.solve_leg()
     Ntype.etas[i] = Ntype.eta
 
-Ptype.current_density = sp.arange(0., -2.6, -0.1) * 100.**2
+Ptype.current_density = sp.arange(0., -2.55, -0.05) * 100.**2
 Ptype.currents = Ptype.current_density * Ptype.area
 Ptype.etas = sp.zeros(sp.size(Ptype.currents))
 for i in sp.arange(sp.size(Ptype.currents)):
@@ -52,42 +52,31 @@ mpl.rcParams['lines.linewidth'] = 1.5
 
 fig1 = mpl.figure()
 mpl.plot(Ntype.current_density / 100.**2, Ntype.etas * 100.,
-         label='N type')
+         label=Ntype.material)
+mpl.twiny()
+mpl.plot(Ptype.current_density / 100.**2, Ptype.etas * 100.,
+         label=Ptype.material)
 mpl.grid()
 mpl.xlabel(r'Current Density ($A/cm^2$)')
 mpl.ylabel(r'$\eta$ (%)')
-mpl.title('Efficiency v. Current\nfor TE Legs')
+mpl.title('Efficiency v. Current\n\n\n')  
 mpl.legend(loc='best')
 fig1.subplots_adjust(bottom=0.12)
 fig1.subplots_adjust(top=0.88)
-mpl.savefig('Plots/eta v current n.pdf')
-mpl.savefig('Plots/eta v current n.png')
+fig1.savefig('Plots/eta v current ex1.pdf')
+fig1.savefig('Plots/eta v current ex1.png')
 
-fig2 = mpl.figure()
-mpl.plot(Ptype.current_density / 100.**2, Ptype.etas * 100.,
-         label='P type')
-mpl.grid()
-mpl.xlabel(r'Current Density ($A/cm^2$)')
-mpl.ylabel(r'$\eta$ (%)')
-mpl.title('Efficiency v. Current\nfor TE Legs')
-mpl.legend(loc='best')
-fig2.subplots_adjust(bottom=0.12)
-fig2.subplots_adjust(top=0.88)
-mpl.savefig('Plots/eta v current p.pdf')
-mpl.savefig('Plots/eta v current p.png')
-
-fig3 = mpl.figure()
-mpl.plot(Ptype.current_density / 100.**2, Ptype.etas * 100.,
-         label='P type')
-mpl.plot(Ntype.current_density / 100.**2, Ntype.etas * 100.,
-         label='N type')
-mpl.grid()
-mpl.xlabel(r'Current Density ($A/cm^2$)')
-mpl.ylabel(r'$\eta$ (%)')
-mpl.title('Efficiency v. Current\nfor TE Legs')
-mpl.legend(loc='best')
-fig3.subplots_adjust(bottom=0.12)
-fig3savefig('Plots/eta v current both.pdf')
-mpl.savefig('Plots/eta v current both.png')
+# fig2 = mpl.figure()
+# mpl.plot(Ptype.current_density / 100.**2, Ptype.etas * 100.,
+#          label='P type')
+# mpl.grid()
+# mpl.xlabel(r'Current Density ($A/cm^2$)')
+# mpl.ylabel(r'$\eta$ (%)')
+# mpl.title('Efficiency v. Current\n' + Ptype.material)  
+# mpl.legend(loc='best')
+# fig2.subplots_adjust(bottom=0.12)
+# fig2.subplots_adjust(top=0.88)
+# fig2.savefig('Plots/eta v current ' + Ptype.material + '.pdf')
+# fig2.savefig('Plots/eta v current ' + Ptype.material + '.png')
 
 mpl.show()
