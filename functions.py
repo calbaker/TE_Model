222240adcd50b47c8c917d321350ae91f88f4b4e
Chad Baker <calbaker@gmail.com>
2011-09-21 13:05:48 -0500

    more robust.  Can handle array of Re.

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 """functions to be used in both exhaust and coolant modules"""
 
+import numpy as np
+
 def set_flow_geometry(self,width):
     self.perimeter = 2.*(self.height+width) # wetted perimeter (m) of
         # exhaust flow
@@ -9,13 +11,24 @@ def set_flow_geometry(self,width):
 
 def set_Re_dependents(self):
     self.set_Re()
-    if (self.Re_D > 2300.).any(): # Do these correlations hold for any tube geometry?
-        self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
-        # flow from Bejan Eq. 8.13
-        self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
-        # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
+    if np.size(self.Re_D) > 1:
+        if (self.Re_D > 2300.).any(): # Do these correlations hold for any tube geometry?
+            self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
+            # flow from Bejan Eq. 8.13
+            self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
+            # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
+        else:
+            self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
+                # parallel plates with constant T
+            self.f = 24./self.Re_D
     else:
-        self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
-            # parallel plates with constant T
-        self.f = 24./self.Re_D
-
+        if (self.Re_D > 2300.): # Do these correlations hold for any tube geometry?
+            self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
+            # flow from Bejan Eq. 8.13
+            self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
+            # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
+        else:
+            self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
+                # parallel plates with constant T
+            self.f = 24./self.Re_D
+        
