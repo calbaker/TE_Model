9effa479fc7aa72324b9c7363b1232318a7a01f2
Chad Baker <calbaker@gmail.com>
2011-09-23 12:25:40 -0500

    Added plot

--- compact_instance.py
+++ compact_instance.py
@@ -51,5 +51,18 @@ plt.rcParams['xtick.labelsize'] = FONTSIZE
 plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 
+plt.figure()
+plt.plot(hx.x_dim * 100., hx.exh.T_nodes, '-r', label='Exhaust')
+plt.plot(hx.x_dim * 100., hx.tem.T_h_nodes, '-g', label='TEM Hot Side')
+plt.plot(hx.x_dim * 100., hx.tem.T_c_nodes, '-k', label='TEM Cold Side')
+plt.plot(hx.x_dim * 100., hx.cool.T_nodes, '-b', label='Coolant')
 
+plt.xlabel('Distance Along HX (cm)')
+plt.ylabel('Temperature (K)')
+#plt.title('Temperature v. Distance, '+hx.type)
+plt.grid()
+plt.legend(loc='best')
+plt.subplots_adjust(bottom=0.15)
+plt.savefig('Plots/temp '+hx.type+'.png')
+plt.savefig('Plots/temp '+hx.type+'.pdf')
 
