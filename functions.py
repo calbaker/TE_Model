e96f4656999bde79d92bc7d4a3f4869a8a8e78d1
Chad Baker <calbaker@gmail.com>
2011-09-26 10:14:27 -0500

    changed formula for f

--- functions.py
+++ functions.py
@@ -13,22 +13,22 @@ def set_Re_dependents(self):
     self.set_Re()
     if np.size(self.Re_D) > 1:
         if (self.Re_D > 2300.).any(): # Do these correlations hold for any tube geometry?
-            self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
-            # flow from Bejan Eq. 8.13
-            self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
+            self.f = 0.316 * self.Re_D**(-1. / 4.) # friction factor for turbulent
+            # flow from FMP 4th Eq. 8.35 
+            self.Nu_D = 0.023 * self.Re_D**(4. / 5.) * self.Pr**(1. / 3.) # Adrian
             # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
         else:
             self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
                 # parallel plates with constant T
-            self.f = 24./self.Re_D
+            self.f = 24. / self.Re_D
     else:
         if (self.Re_D > 2300.): # Do these correlations hold for any tube geometry?
-            self.f = 0.078*self.Re_D**(-1./4.) # friction factor for turbulent
-            # flow from Bejan Eq. 8.13
-            self.Nu_D = 0.023*self.Re_D**(4/5.)*self.Pr**(1/3.) # Adrian
+            self.f = 0.316 * self.Re_D**(-1. / 4.) # friction factor for turbulent
+            # flow from FMP 4th Eq. 8.35 
+            self.Nu_D = 0.023 * self.Re_D**(4. / 5.)*self.Pr**(1. / 3.) # Adrian
             # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
         else:
             self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
                 # parallel plates with constant T
-            self.f = 24./self.Re_D
+            self.f = 24. / self.Re_D
         
