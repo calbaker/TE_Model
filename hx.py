2b892d4c380dffa564cb9ce0a6bc45355203e166
Chad Baker <calbaker@gmail.com>
2011-09-30 14:06:58 -0500

    fixed overall heat transfer coefficient

--- hx.py
+++ hx.py
@@ -104,13 +104,16 @@ class HX(object):
         # TE stuff
 
         self.U = ( (self.exh.R_thermal + self.plate.R_thermal +
-        self.R_thermal + self.tem.R_thermal + self.R_thermal +
-        self.plate.R_thermal + self.cool.R_thermal )**-1 )  
+        self.R_thermal + self.plate.R_thermal + self.tem.R_thermal +
+        self.plate.R_thermal + self.R_thermal + self.plate.R_thermal +
+        self.cool.R_thermal )**-1 )   
         # overall heat transfer coefficient (kW/m^2-K)
-        self.U_hot = (self.exh.R_thermal + self.plate.R_thermal)**-1
+        self.U_hot = ( (self.exh.R_thermal + self.plate.R_thermal +
+        self.R_thermal)**-1 )
         # heat transfer coefficient (kW/m^-K) between TE hot side and
         # exhaust  
-        self.U_cold = (self.cool.R_thermal + self.plate.R_thermal)**-1
+        self.U_cold = ( (self.cool.R_thermal +
+        self.plate.R_thermal + self.R_thermal)**-1 )
         # heat transfer coefficient (kW/m^-K) between TE cold side and
         # coolant  
         
