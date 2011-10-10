60aac6494ce45d0bb8fb12cca9c576f4a4f44980
Chad Baker <calbaker@gmail.com>
2011-10-10 16:44:37 -0500

    replaced power with multiply for speed.  didn't make much difference.

--- tem.py
+++ tem.py
@@ -87,9 +87,9 @@ class Leg():
             (self.J * self.T[j-1] * self.alpha - self.q[j-1]) )
             # determines temperature of current segment based on
             # properties evaluated at previous segment
-            self.dq = ( (self.rho * self.J**2 * (1 + self.alpha**2 *
-            self.T[j-1] / (self.rho * self.k)) - self.J * self.alpha *
-            self.q[j-1] / self.k) )
+            self.dq = ( (self.rho * self.J * self.J * (1 + self.alpha
+        * self.alpha * self.T[j-1] / (self.rho * self.k)) - self.J *
+        self.alpha * self.q[j-1] / self.k) ) 
             self.q[j] = ( self.q[j-1] + self.dq * self.segment_length )
             self.V_segment[j] = ( self.alpha * (self.T[j] -
             self.T[j-1]) )
