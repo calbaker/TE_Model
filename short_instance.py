41e32f245686c0c588a8a3e6fefb58d154535870
Chad Baker <calbaker@gmail.com>
2011-10-12 14:50:39 -0500

    first commit

---
+++ short_instance.py
@@ -0,0 +1,72 @@
+# Chad Baker
+# Created on 2011 Feb 10
+
+# Distribution Modules
+import matplotlib.pyplot as plt
+import numpy as np
+import xlrd
+
+# User Defined Modules
+# In this directory
+import hx
+reload(hx)
+
+print "Beginning execution..."
+
+length = 1 / 1000.
+current = 4.5
+area = (0.002)**2
+area_ratio = 1.25 # p-type area per n-type area
+
+hx = hx.HX()
+hx.nodes = 1.  
+hx.thermoelectrics_on = True
+hx.width = 9.e-2
+hx.length = 0.05
+hx.exh.bypass = 0.
+hx.exh.height = 1.e-2
+Vdot_cool = 4. # coolant flow rate (GPM) 
+mdot_cool = 4. / 60. * 3.8 / 1000. * hx.cool.rho  
+hx.cool.mdot = mdot_cool / 60. * 3.8
+hx.cool.height = 0.5e-2
+hx.tem.I = 4.5
+hx.tem.length = length
+hx.tem.Ntype.material = 'MgSi'
+hx.tem.Ntype.area = area
+hx.tem.Ptype.material = 'HMS'
+hx.tem.Ptype.area = area * 2. 
+hx.tem.area_void = 25. * area
+hx.type = 'counter'
+hx.exh.P = 100.
+
+hx.cool.T_outlet = 300.
+hx.exh.T_inlet = 500.
+hx.set_mdot_charge()
+hx.solve_hx()
+
+print "\nProgram finished."
+
+# Plot configuration
+FONTSIZE = 20
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+
+plt.figure()
+plt.plot(hx.x_dim * 100., hx.exh.T_nodes, '-r', label='Exhaust')
+plt.plot(hx.x_dim * 100., hx.tem.T_h_nodes, '-g', label='TEM Hot Side')
+plt.plot(hx.x_dim * 100., hx.tem.T_c_nodes, '-k', label='TEM Cold Side')
+plt.plot(hx.x_dim * 100., hx.cool.T_nodes, '-b', label='Coolant')
+
+plt.xlabel('Distance Along HX (cm)')
+plt.ylabel('Temperature (K)')
+#plt.title('Temperature v. Distance, '+hx.type)
+plt.grid()
+plt.legend(loc='best')
+plt.subplots_adjust(bottom=0.15)
+plt.savefig('Plots/temp '+hx.type+'.png')
+plt.savefig('Plots/temp '+hx.type+'.pdf')
+
