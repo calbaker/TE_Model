9b84bbd10036727be1c3168e8ab2d9450f1acdec
Chad Baker <calbaker@gmail.com>
2011-10-14 15:53:29 -0500

    first commit

---
+++ anal_max_eta.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy.optimize as spopt
+
+alpha_p = 
+
+def get_eta(R, I, L):
+    
+    eta_t = ( I**2 * R * (alpha_p - alpha_n) * T_h * I + (T_h - T_c) /
+    L * (A_p * k_p + A_n * k_n) - I**2 * L * 0.5 * (rho_p / A_p +
+    rho_n / A_n) ) 
+
+    return eta_t
+
+  
