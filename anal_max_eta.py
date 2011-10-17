86540d5ddc4fd5a3ae879141d481040e1890cae6
Chad Baker <calbaker@gmail.com>
2011-10-17 14:28:24 -0500

    fixed a typo in the formula.  might need to play around with load resistance.

--- anal_max_eta.py
+++ anal_max_eta.py
@@ -36,9 +36,9 @@ def get_eta(x):
     L = x[2]
 
     A_p = A_n * R_a
-    eta_t = ( I**2 * R / (alpha_p - alpha_n) * T_h * I + (T_h - T_c) /
+    eta_t = ( I**2 * R / ((alpha_p - alpha_n) * T_h * I + (T_h - T_c) /
     L * (A_p * k_p + A_n * k_n) - I**2 * L * 0.5 * (rho_p / A_p +
-    rho_n / A_n) ) 
+    rho_n / A_n)) ) 
 
     return -eta_t
 
