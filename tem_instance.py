c8bd61e6f36278956790cac3a95dfd6b50163e19
Chad Baker <calbaker@gmail.com>
2011-10-18 16:15:36 -0500

    reload(tem)

--- tem_instance.py
+++ tem_instance.py
@@ -5,6 +5,7 @@ import time
 
 # local user modules
 import tem
+reload(tem)
 
 t0 = time.clock()
 
@@ -31,16 +32,16 @@ tem.Ntype.set_prop_fit()
 tem.solve_tem()
 
 length1d = np.linspace(0.01, 3, 25) / 1000.
-current1d = np.linspace(0.01, 8, 25)
-area_ratio1d = np.linspace(0.5, 3, 25)
+current1d = np.linspace(0.01, 8, 26)
+area_ratio1d = np.linspace(0.5, 3, 27)
 
 length_current, current_length = np.meshgrid(length1d, current1d)
 current_area, area_current = np.meshgrid(current1d, area_ratio1d)
 length_area, area_length = np.meshgrid(length1d, area_ratio1d)
 
-P_length_current = np.empty([np.size(length1d), np.size(current1d)]) 
-P_current_area = np.empty([np.size(current1d), np.size(area_ratio1d)]) 
-P_length_area = np.empty([np.size(length1d), np.size(area_ratio1d)]) 
+eta_length_current = np.empty([np.size(length1d), np.size(current1d)]) 
+eta_current_area = np.empty([np.size(current1d), np.size(area_ratio1d)]) 
+eta_length_area = np.empty([np.size(length1d), np.size(area_ratio1d)]) 
 
 for i in range(np.size(length1d)):
     tem.length = length1d[i]
@@ -48,7 +49,7 @@ for i in range(np.size(length1d)):
         tem.I = current1d[j]
         tem.set_constants()
         tem.solve_tem()
-        P_length_current[i,j] = tem.P
+        eta_length_current[i,j] = tem.eta
 
 tem.length = length
 tem.current = current
@@ -61,7 +62,7 @@ for i in range(np.size(current1d)):
         tem.Ptype.area = tem.area - tem.Ntype.area 
         tem.set_constants()
         tem.solve_tem()
-        P_current_area[i,j] = tem.P
+        eta_current_area[i,j] = tem.eta
 
 tem.current = current
 tem.area_ratio = area_ratio
@@ -76,7 +77,7 @@ for i in range(np.size(length1d)):
         tem.Ptype.area = tem.area - tem.Ntype.area 
         tem.set_constants()
         tem.solve_tem()
-        P_length_area[i,j] = tem.P
+        eta_length_area[i,j] = tem.eta
 
 tem.length = length
 tem.current = current
@@ -96,27 +97,36 @@ plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 plt.rcParams['axes.formatter.limits'] = -3,3
 
+
+LEVELS1 = np.linspace(0, eta_length_current.max(), 15)
 fig1 = plt.figure()
-FCS = plt.contourf(length_current * 1000., current_length, P_length_current.T) 
+FCS = plt.contourf(length_current * 1000., current_length,
+                   eta_length_current.T, levels = LEVELS1)
 CB = plt.colorbar(FCS, orientation='vertical')
+CB.set_label('TE Thermal Efficiency')
 plt.grid()
 plt.xlabel("Leg Height (mm)")
 plt.ylabel("Current (A)")
 fig1.savefig('Plots/TE Optimization/length_current.pdf')
 fig1.savefig('Plots/TE Optimization/length_current.png')
 
+LEVELS2 = np.linspace(0, eta_length_area.max(), 15)
 fig2 = plt.figure()
-FCS = plt.contourf(length_area * 1000., area_length, P_length_area.T) 
+FCS = plt.contourf(length_area * 1000., area_length, eta_length_area.T,
+                   levels=LEVELS2)  
 CB = plt.colorbar(FCS, orientation='vertical')
+CB.set_label('TE Thermal Efficiency')
 plt.grid()
 plt.xlabel("Leg Height (mm)")
 plt.ylabel("P-type to N-type Area Ratio")
 fig2.savefig('Plots/TE Optimization/length_area.pdf')
 fig2.savefig('Plots/TE Optimization/length_area.png')
 
+LEVELS3 = np.linspace(0, eta_current_area.max(), 15)
 fig3 = plt.figure()
-FCS = plt.contourf(current_area, area_current, P_current_area.T) 
+FCS = plt.contourf(current_area, area_current, eta_current_area.T) 
 CB = plt.colorbar(FCS, orientation='vertical')
+CB.set_label('TE Thermal Efficiency')
 plt.grid()
 plt.xlabel("Current (A)")
 plt.ylabel("P-type to N-type Area Ratio")
