764b3f62f64f7269db031dc9ed8061bfcdbd14b9
Chad Baker <calbaker@gmail.com>
2011-10-18 10:48:38 -0500

    now it's 3 2d surfaces instead of a cube

--- anal_max_eta.py
+++ anal_max_eta.py
@@ -36,40 +36,35 @@ pn_area_ratio = np.linspace(0.2,2,50)
 I = np.linspace(0.1,5,50)
 L = np.linspace(0.1,4,50) * 0.001
 
+pn_area_ratio0 = 1.
+I0 = 4.
+L0 = 0.001
+
 pn_area_ratio2d_I, I2d_pn_area_ratio = np.meshgrid(pn_area_ratio,I)
 pn_area_ratio2d_L, L2d_pn_area_ratio = np.meshgrid(pn_area_ratio,L)
 I2d_L, L2d_I = np.meshgrid(I,L)
 
-eta_t = np.empty([np.size(pn_area_ratio), np.size(I), np.size(L)]) 
+eta_t_ij = np.empty([np.size(pn_area_ratio), np.size(I)])
+eta_t_jk = np.empty([np.size(I), np.size(L)])
+eta_t_ik = np.empty([np.size(pn_area_ratio), np.size(L)])
 
 for i in range(np.size(pn_area_ratio)):
     for j in range(np.size(I)):
-        for k in range(np.size(L)):
-            x = np.array([pn_area_ratio[i], I[j], L[k]])
-            eta_t[i,j,k] = -get_eta(x)
+        x = np.array([pn_area_ratio[i], I[j], L0])
+        eta_t_ij[i,j] = -get_eta(x)
 
-fig1 = plt.figure()
-FCS = plt.contourf(L2d_I * 1000., I2d_L, eta_t[25,:,:].T) 
-CB = plt.colorbar(FCS, orientation='vertical')
-CB.set_label('TE Thermal Efficiency')
-plt.grid()
-plt.xlabel("Leg Height (mm)")
-plt.ylabel("Current (A)")
-fig1.savefig('Plots/HX Optimization/length_current anal.pdf')
-fig1.savefig('Plots/HX Optimization/length_current anal.png')
+for j in range(np.size(I)):
+    for k in range(np.size(L)):
+        x = np.array([pn_area_ratio0, I[j], L[k]])
+        eta_t_jk[j,k] = -get_eta(x)
 
-fig2 = plt.figure()
-FCS = plt.contourf(L2d_pn_area_ratio * 1000., pn_area_ratio2d_L, eta_t[:,25,:].T) 
-CB = plt.colorbar(FCS, orientation='vertical')
-CB.set_label('TE Thermal Efficiency')
-plt.grid()
-plt.xlabel("Leg Height (mm)")
-plt.ylabel("Area Ratio")
-fig2.savefig('Plots/HX Optimization/length_fill anal.pdf')
-fig2.savefig('Plots/HX Optimization/length_fill anal.png')
+for i in range(np.size(pn_area_ratio)):
+    for k in range(np.size(L)):
+        x = np.array([pn_area_ratio[i], I0, L[k]])
+        eta_t_ik[i,j] = -get_eta(x)
 
 fig3 = plt.figure()
-FCS = plt.contourf(I2d_pn_area_ratio, pn_area_ratio2d_I, eta_t[:,:,25].T) 
+FCS = plt.contourf(pn_area_ratio2d_I, I2d_pn_area_ratio, eta_t_ij.T) 
 CB = plt.colorbar(FCS, orientation='vertical')
 CB.set_label('TE Thermal Efficiency')
 plt.grid()
@@ -78,3 +73,24 @@ plt.ylabel("Area Ratio")
 fig3.savefig('Plots/HX Optimization/current_fill anal.pdf')
 fig3.savefig('Plots/HX Optimization/current_fill anal.png')
 
+fig1 = plt.figure()
+FCS = plt.contourf(I2d_L, L2d_I * 1000., eta_t_jk.T) 
+CB = plt.colorbar(FCS, orientation='vertical')
+CB.set_label('TE Thermal Efficiency')
+plt.grid()
+plt.xlabel("Leg Height (mm)")
+plt.ylabel("Current (A)")
+fig1.savefig('Plots/HX Optimization/.pdf')
+fig1.savefig('Plots/HX Optimization/.png')
+
+fig2 = plt.figure()
+FCS = plt.contourf(pn_area_ratio2d_L, L2d_pn_area_ratio * 1000., eta_t_ik.T) 
+CB = plt.colorbar(FCS, orientation='vertical')
+CB.set_label('TE Thermal Efficiency')
+plt.grid()
+plt.xlabel("Leg Height (mm)")
+plt.ylabel("Area Ratio")
+fig2.savefig('Plots/HX Optimization/length_fill anal.pdf')
+fig2.savefig('Plots/HX Optimization/length_fill anal.png')
+
+plt.show()
