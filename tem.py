04b74b6f01276ca1d40e2a23c15ef5a5448db69e
Chad Baker <calbaker@gmail.com>
2011-10-24 13:47:33 -0500

    bout to change formula for xi

--- tem.py
+++ tem.py
@@ -234,10 +234,12 @@ class TEModule():
         self.set_eta_max()
         self.set_A_opt()
         delta_T = self.T_h - self.T_c
+
         a = ( (self.Ptype.rho + self.Ntype.rho / self.A_opt) * (1. -
         self.eta_max / 2.) / self.Ptype.area )
         b = ( (self.Ptype.alpha - self.Ntype.alpha / self.A_opt) *
         (self.eta_max * self.T_h - delta_T) / self.Ptype.area )
         c = ( self.eta_max * delta_T * (self.Ptype.k + self.Ntype.k *
         self.A_opt) * self.Ptype.area )
+
         self.xi = ( (-b + np.sqrt(b**2 - 4. * a * c)) / (2. * a) )
