6716bc8d590874115dea4994fa8ddfc2bae30563
Chad Baker <calbaker@gmail.com>
2011-10-26 11:47:54 -0500

    yep

--- tem_instance.py
+++ tem_instance.py
@@ -33,14 +33,23 @@ tem.Ptype.set_prop_fit()
 tem.Ntype.set_prop_fit()
 tem.solve_tem()
 
-T_props = np.linspace(300,600.,100)
+T_props = np.linspace(300,450.,100)
+T_h_goal = np.linspace(300,600.,100)
 A_opt = np.empty(np.size(T_props))
+xi_opt = np.empty(np.size(T_props))
+eta_max = np.empty(np.size(T_props))
+
 for i in range(np.size(T_props)):
     tem.T_props = T_props[i]
-    tem.set_TEproperties()
-    tem.set_eta_max()
     tem.set_A_opt()
     A_opt[i] = tem.A_opt
+
+for i in range(np.size(T_h_goal)):
+    tem.T_h_goal = T_h_goal[i]
+    tem.set_xi()
+    xi_opt[i] = tem.xi
+    tem.set_eta_max()
+    eta_max[i] = tem.eta_max
 
 # Plot configuration
 FONTSIZE = 15
@@ -59,7 +68,27 @@ fig1 = plt.figure()
 plt.plot(T_props, A_opt, '-k')
 plt.xlabel('Property Evaluation Temperature (K)')
 plt.ylabel(r'Optimal $\frac{A_n}{A_p}$')
+plt.ylim(0,0.75)
 plt.grid()
+plt.subplots_adjust(left=0.15)
 plt.savefig('Plots/area_ratio_v_T.pdf')
 plt.savefig('Plots/area_ratio_v_T.png')
+
+fig2 = plt.figure()
+plt.plot(T_h_goal, xi_opt, '-k')
+plt.xlabel('TE Hot Side Temperature (K)')
+plt.ylabel(r'Optimal $\xi=IL$')
+plt.grid()
+plt.subplots_adjust(left=0.15)
+plt.savefig('Plots/xi_v_T.pdf')
+plt.savefig('Plots/xi_v_T.png')
+
+fig3 = plt.figure()
+plt.plot(T_h_goal, eta_max*100., '-k')
+plt.xlabel('TE Hot Side Temperature (K)')
+plt.ylabel(r'$\eta_{max}$(%)')
+plt.grid()
+plt.subplots_adjust(left=0.15)
+plt.savefig('Plots/eta_max_v_T.pdf')
+plt.savefig('Plots/eta_max_v_T.png')
 
