021d5788e0dbb6b0e31297c3cbf2717735d5d149
Chad Baker <calbaker@gmail.com>
2011-10-27 15:58:45 -0500

    fixing levels on p plots

--- tem_instances.py
+++ tem_instances.py
@@ -36,6 +36,7 @@ tem.Ntype.set_prop_fit()
 tem.solve_tem()
 tem.T_props = 0.5 * (tem.T_h + tem.T_c)
 tem.set_eta_max()
+tem.set_power_max()
 tem.set_A_opt()
 
 res = 40
@@ -51,6 +52,10 @@ eta_length_current = np.empty([np.size(length1d), np.size(current1d)])
 eta_current_area = np.empty([np.size(current1d), np.size(area_ratio1d)]) 
 eta_length_area = np.empty([np.size(length1d), np.size(area_ratio1d)]) 
 
+p_length_current = np.empty([np.size(length1d), np.size(current1d)]) 
+p_current_area = np.empty([np.size(current1d), np.size(area_ratio1d)]) 
+p_length_area = np.empty([np.size(length1d), np.size(area_ratio1d)]) 
+
 for i in range(np.size(length1d)):
     tem.length = length1d[i]
     for j in range(np.size(current1d)):
@@ -58,6 +63,7 @@ for i in range(np.size(length1d)):
         tem.set_constants()
         tem.solve_tem()
         eta_length_current[i,j] = tem.eta
+        p_length_current[i,j] = tem.P
 
 tem.length = length
 tem.I = current
@@ -71,6 +77,7 @@ for i in range(np.size(current1d)):
         tem.set_constants()
         tem.solve_tem()
         eta_current_area[i,j] = tem.eta
+        p_current_area[i,j] = tem.P
 
 tem.length = length
 tem.I = current
@@ -86,6 +93,7 @@ for i in range(np.size(length1d)):
         tem.set_constants()
         tem.solve_tem()
         eta_length_area[i,j] = tem.eta
+        p_length_area[i,j] = tem.P
 
 tem.length = length
 tem.I = current
@@ -110,23 +118,28 @@ plt.rcParams['lines.linewidth'] = 2.5
 plt.rcParams['lines.markersize'] = 10
 plt.rcParams['axes.formatter.limits'] = -3,3
 
-dummy = ( tem.eta_max * 100. - np.logspace(np.log10(tem.eta_max *
-                                                      100), -1, 10) )
+high = tem.eta_max * 100.
+dummy = ( high - np.logspace(np.log10(high), -1, 10) )
 LEVELS_eta = np.empty(np.size(dummy)+1)
 LEVELS_eta[-1] = tem.eta_max * 100.
 LEVELS_eta[0:-1] = dummy
+
+high = 0.1
+dummy = ( high - np.logspace(np.log10(high), -1, 10) )
+LEVELS_p = np.empty(np.size(dummy)+1)
+LEVELS_p[-1] = tem.power_max * 1000.
+LEVELS_p[0:-1] = dummy
 
 fig1 = plt.figure()
 FCS = plt.contourf(current_length, length_current * 1000.,
                    eta_length_current.T * 100., levels=LEVELS_eta)
 CB = plt.colorbar(FCS, orientation='vertical', format="%.2f")
 CB.set_label('TE Thermal Efficiency (%)')
-plt.ylim(ymax=length1d.max()*1000.)
 plt.grid()
 plt.ylabel("Leg Height (mm)")
 plt.xlabel("Current (A)")
-fig1.savefig('Plots/' + tem.method + '/length_current.pdf')
-fig1.savefig('Plots/' + tem.method + '/length_current.png')
+fig1.savefig('Plots/' + tem.method + '/eta_length_current.pdf')
+fig1.savefig('Plots/' + tem.method + '/eta_length_current.png')
 
 fig2 = plt.figure()
 FCS = plt.contourf(area_length, length_area * 1000., 
@@ -136,8 +149,8 @@ CB.set_label('TE Thermal Efficiency (%)')
 plt.grid()
 plt.ylabel("Leg Height (mm)")
 plt.xlabel("P-type to N-type Area Ratio")
-fig2.savefig('Plots/' + tem.method + '/length_area.pdf')
-fig2.savefig('Plots/' + tem.method + '/length_area.png')
+fig2.savefig('Plots/' + tem.method + '/eta_length_area.pdf')
+fig2.savefig('Plots/' + tem.method + '/eta_length_area.png')
 
 fig3 = plt.figure()
 FCS = plt.contourf(area_current, current_area, eta_current_area.T * 100.,
@@ -147,8 +160,41 @@ CB.set_label('TE Thermal Efficiency (%)')
 plt.grid()
 plt.ylabel("Current (A)")
 plt.xlabel("P-type to N-type Area Ratio")
-fig3.savefig('Plots/' + tem.method + '/current_area.pdf')
-fig3.savefig('Plots/' + tem.method + '/current_area.png')
+fig3.savefig('Plots/' + tem.method + '/eta_current_area.pdf')
+fig3.savefig('Plots/' + tem.method + '/eta_current_area.png')
+
+fig1p = plt.figure()
+FCS = plt.contourf(current_length, length_current * 1000.,
+                   p_length_current.T * 1000., levels=LEVELS_p)
+CB = plt.colorbar(FCS, orientation='vertical', format="%.2f")
+CB.set_label('TE Power (W)')
+plt.grid()
+plt.ylabel("Leg Height (mm)")
+plt.xlabel("Current (A)")
+fig1p.savefig('Plots/' + tem.method + '/p_length_current.pdf')
+fig1p.savefig('Plots/' + tem.method + '/p_length_current.png')
+
+fig2 = plt.figure()
+FCS = plt.contourf(area_length, length_area * 1000., 
+                   p_length_area.T * 1000., levels=LEVELS_p)   
+CB = plt.colorbar(FCS, orientation='vertical', format="%.2f")
+CB.set_label('TE Power (W)')
+plt.grid()
+plt.ylabel("Leg Height (mm)")
+plt.xlabel("P-type to N-type Area Ratio")
+fig2.savefig('Plots/' + tem.method + '/p_length_area.pdf')
+fig2.savefig('Plots/' + tem.method + '/p_length_area.png')
+
+fig3 = plt.figure()
+FCS = plt.contourf(area_current, current_area, p_current_area.T * 1000.,
+                   levels=LEVELS_p) 
+CB = plt.colorbar(FCS, orientation='vertical', format="%.2f")
+CB.set_label('TE Power (W)')
+plt.grid()
+plt.ylabel("Current (A)")
+plt.xlabel("P-type to N-type Area Ratio")
+fig3.savefig('Plots/' + tem.method + '/p_current_area.pdf')
+fig3.savefig('Plots/' + tem.method + '/p_current_area.png')
 
 # plt.show()
 
