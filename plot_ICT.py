ed01affb9044e1b73942820a906bff33f39f3e24
Chad Baker <calbaker@gmail.com>
2011-11-01 18:25:48 -0500

    ready

--- plot_ICT.py
+++ plot_ICT.py
@@ -17,15 +17,15 @@ plt.plot(hx_inst.x_dim * 100., hx_inst.tem.T_h_nodes, '-g', label='TEM Hot Side'
 plt.plot(hx_inst.x_dim * 100., hx_inst.tem.T_c_nodes, '-k', label='TEM Cold Side')
 plt.plot(hx_inst.x_dim * 100., hx_inst.cool.T_nodes, '-b', label='Coolant')
 
-plt.plot(hx_fins.x_dim * 100., hx_fins.exh.T_nodes, '--r')
-plt.plot(hx_fins.x_dim * 100., hx_fins.tem.T_h_nodes, '--g')
-plt.plot(hx_fins.x_dim * 100., hx_fins.tem.T_c_nodes, '--k')
-plt.plot(hx_fins.x_dim * 100., hx_fins.cool.T_nodes, '--b')
+plt.plot(hx_fins0.x_dim * 100., hx_fins0.exh.T_nodes, '--r')
+plt.plot(hx_fins0.x_dim * 100., hx_fins0.tem.T_h_nodes, '--g')
+plt.plot(hx_fins0.x_dim * 100., hx_fins0.tem.T_c_nodes, '--k')
+plt.plot(hx_fins0.x_dim * 100., hx_fins0.cool.T_nodes, '--b')
 
-plt.plot(hx_ducts.x_dim * 100., hx_ducts.exh.T_nodes, ':r')
-plt.plot(hx_ducts.x_dim * 100., hx_ducts.tem.T_h_nodes, ':g')
-plt.plot(hx_ducts.x_dim * 100., hx_ducts.tem.T_c_nodes, ':k')
-plt.plot(hx_ducts.x_dim * 100., hx_ducts.cool.T_nodes, ':b')
+plt.plot(hx_ducts0.x_dim * 100., hx_ducts0.exh.T_nodes, ':r')
+plt.plot(hx_ducts0.x_dim * 100., hx_ducts0.tem.T_h_nodes, ':g')
+plt.plot(hx_ducts0.x_dim * 100., hx_ducts0.tem.T_c_nodes, ':k')
+plt.plot(hx_ducts0.x_dim * 100., hx_ducts0.cool.T_nodes, ':b')
 
 plt.xlabel('Distance Along HX (cm)')
 plt.ylabel('Temperature (K)')
