7582548328ad9699c9e02597bb970ebf23577628
Chad Baker <calbaker@gmail.com>
2011-11-07 20:19:32 -0600

    a bit more fancy

--- platewall.py
+++ platewall.py
@@ -4,9 +4,16 @@
 
 class PlateWall(object):
     """class for modeling metal walls of heat exchanger"""
-    k = 0.2 # thermal conductivity (kW/m-K) of Aluminum HX plate
-    t = 0.00635 # thickness (m) of HX plate
+
+    def __init__(self):
+        """Initializes material properties and plate wall geometry defaults."""
+        self.k = 0.2 # thermal conductivity (kW/m-K) of Aluminum HX plate
+        self.t = 0.00635 # thickness (m) of HX plate
+        self.c_p = 900. # specific heat of aluminum (J/kg-K)
+
     def set_h(self):
+        """Sets the effective convection coefficient which is the
+        inverse of thermal resistance."""
         self.h = self.k/self.t
-        self.R_thermal = 1 / self.h
+        self.R_thermal = 1. / self.h
 
