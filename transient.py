c4949264eb18d1d252e63cbb218d8d991d932006
Chad Baker <calbaker@gmail.com>
2011-11-08 16:45:54 -0600

    yep

--- transient.py
+++ transient.py
@@ -12,46 +12,8 @@ import os
 import hx
 reload(hx)
 
-area = (0.002)**2
-length = 1.e-3
-current = 4.
-area_ratio = 0.69
-fill_fraction = 1. / 40.
+class transient_hx(hx):
+    """Special class for modeling waste heat recovery heat exchanger
+    with transient inlet temperature and flow conditions."""
 
-hx1 = hx.HX()
-hx1.tem.method = 'analytical'
-hx1.width = 30.e-2
-hx1.exh.bypass = 0.
-hx1.exh.height = 3.5e-2
-hx1.cool.mdot = 1.
-hx1.length = 1.
-hx1.tem.I = current
-hx1.tem.length = length
-
-hx1.tem.Ptype.material = 'HMS'
-hx1.tem.Ntype.material = 'MgSi'
-
-hx1.tem.Ptype.area = area                           
-hx1.tem.Ntype.area = hx1.tem.Ptype.area * area_ratio
-hx1.tem.area_void = ( (1. - fill_fraction) / fill_fraction *
-                           (hx1.tem.Ptype.area +
-                            hx1.tem.Ntype.area) )  
-
-hx1.type = 'parallel'
-
-hx1.exh.T_inlet = 800.
-hx1.exh.P = 100.
-hx1.cool.T_inlet = 300.
-
-hx1.set_mdot_charge()
-
-offset = 700.
-amplitude = 100.
-
-hx1.exh.T_inlet_array = ( offset + amplitude *
-                          np.linspace(0, 2.*np.pi, 25) )
-
-for i in range(np.size(hx1.exh.T_inlet_array)):
-    hx1.exh.T_inlet = hx1.exh.T_inlet_array[i]
-    hx1.solve_hx()
-
+    
