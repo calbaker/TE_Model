57f3e0040091e877897e7b97e17dc57bbea0efc3
Chad Baker <calbaker@gmail.com>
2011-11-09 10:28:56 -0600

    really removed platewall class definition this time.

--- hx.py
+++ hx.py
@@ -20,15 +20,6 @@ import platewall
 reload(platewall)
 
 
-class PlateWall(object):
-    """class for modeling metal walls of heat exchanger"""
-    k = 0.2 # thermal conductivity (kW/m-K) of Aluminum HX plate
-    t = 0.00635 # thickness (m) of HX plate
-    def set_h(self):
-        self.h = self.k/self.t
-        self.R_thermal = 1 / self.h
-
-
 class HX(object):
     """class for handling HX system"""
 
