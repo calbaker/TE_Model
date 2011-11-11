2faa3db3a5e77e768a6febb969f41bca0887edc2
Chad Baker <calbaker@gmail.com>
2011-11-11 15:37:26 -0600

    almost got it.

--- Modules/platewall.py
+++ Modules/platewall.py
@@ -13,9 +13,8 @@ class PlateWall(object):
         self.thickness = 0.00635 # thickness (m) of HX plate
         self.R_contact = 0.
         # thermal contact resistance (m^2*K/kW) between plates
-        self.nodes = 3.
-        self.time = 1. # time (s) of something that I don't understand
-                       # yet.  
+        self.nodes = 3. # default number of nodes in transient
+                        # solution.  
         self.t_step = 0.01 # time step (s) in transient solution
 
     def set_h(self):
@@ -29,33 +28,40 @@ class PlateWall(object):
         self.T0 = np.linspace(self.T_c, self.T_h, self.nodes)
 
     def setup_transient(self):
-        """sets Fo and maybe other things."""
-        self.x_step = self.thickness / (self.nodes - 1.)
-        self.Fo = ( self.alpha * self.t_step / self.x_step**2)         
-        self.t_crit = self.x_step**2 / (2. * self.alpha)
-        self.margin = (t_crit - t_step) / t_crit * 100.
-        if self.t_step > self.t_crit:
+        """sets Fo and maybe other things.  See Mills Table 3.8""" 
+        self.x_step = self.thickness / (self.nodes - 1.) 
+        self.Fo = ( self.alpha * self.t_step / self.x_step**2) # Fourier number 
+        self.Bi = ( self.h * self.thickness / self.k ) # Biot number 
+        self.Fo_crit = 1. / (2. * (1. + self.Bi))
+        self.margin = (Fo_crit - Fo) / Fo_crit * 100.
+        if self.Fo > self.Fo_crit:
             print "time step is", margin, """percent lower than
         necessary."""
         self.T = np.zeros([self.x.size, self.t.size])
-
-    def solve_transient(self):
-        """Similar to tem.solve_leg but simpler."""
-        self.T_old = self.T
-
-        T[:,0] = np.array(np.linspace(T_hot[0], T_cold, x.size))
-        T[-1,:] = T_cold
+        self.T[:,0] = np.array(np.linspace(self.T_h, self.T_c,
+        self.x.size)) 
 
         # creating and populating the coefficient matrix
-        coeff_mat = np.zeros([T.shape[0], T.shape[0]])
-        coeff_mat[0,0] = 1.
-        coeff_mat[-1,-1] = 1.
+        self.coeff_mat = np.zeros([self.T.shape[0], self.T.shape[0]]) 
+        self.coeff_mat[0,0] = 1.
+        self.coeff_mat[-1,-1] = 1.
         for pop in range(coeff_mat.shape[0]-2):
-            coeff_mat[pop+1, pop] = Fo
-            coeff_mat[pop+1, pop+1] = 1. - 2. * Fo
-            coeff_mat[pop+1, pop+2] = Fo
+            self.coeff_mat[pop+1, pop] = self.Fo
+            self.coeff_mat[pop+1, pop+1] = 1. - 2. * self.Fo 
+            self.coeff_mat[pop+1, pop+2] = self.Fo
+        self.coeff_mat2 = np.zeros(self.T.shape[0])
+        self.coeff_mat2[0,0] = 2. * self.Fo * self.Bi
+        self.coeff_mat2[-1, -1] = 1.
+            
+    def solve_transient(self, T_exh, T_te_hot):
+        """Similar to tem.solve_leg but simpler and maybe not
+        simpler.""" 
+        self.T_old = self.T
+        self.T_bc = np.array([T_exh, 0., T_te_hot)
+        # forcing matrix
 
             # solving
         for i in range(1,t.size):
-            T[:,i] = np.dot(coeff_mat, T[:,i-1])
+            self.T[:,i] = ( np.dot(self.coeff_mat, self.T[:,i-1])
+                            np.dot(self.coeff_mat2, self.T_bc) )
 
