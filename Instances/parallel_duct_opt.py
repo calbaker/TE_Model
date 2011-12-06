f2947844767d4d60fee21223f9e3035d379517d8
Chad Baker <calbaker@gmail.com>
2011-12-06 16:16:49 -0600

    works, I think, mostly.  needs to account for TE device height

--- Instances/parallel_duct_opt.py
+++ Instances/parallel_duct_opt.py
@@ -44,7 +44,7 @@ hx_ducts.tem.area_void = ( (1. - fill_fraction) / fill_fraction *
                            (hx_ducts.tem.Ptype.area +
                             hx_ducts.tem.Ntype.area) )  
 
-# hx_ducts.tem.method = 'analytical'
+hx_ducts.tem.method = 'analytical'
 hx_ducts.type = 'counter'
 
 hx_ducts.exh.T_inlet = 800.
@@ -52,30 +52,53 @@ hx_ducts.exh.P = 100.
 hx_ducts.cool.T_outlet = 310.
 hx_ducts.cool.T_inlet_set = 300.
 
-ducts = np.arange(2, 18, 1)
-
-hx_ducts.exh.height_array = 3.5e-2 / ducts
-hx_ducts.cool.height_array = 2.e-2 / (ducts + 1.)
+hx_ducts.ducts = np.arange(2, 18, 1)
 
 hx_ducts.set_mdot_charge()
 hx_ducts.exh.mdot0 = hx_ducts.exh.mdot 
 
-hx_ducts.exh.mdot_array = hx_ducts.exh.mdot0 / ducts
-hx_ducts.cool.mdot_array = hx_ducts.cool.mdot * 2. / (ducts + 1.) 
+hx_ducts.exh.mdot_array = hx_ducts.exh.mdot0 / hx_ducts.ducts
+hx_ducts.cool.mdot_array = hx_ducts.cool.mdot * 2. / (hx_ducts.ducts + 1.) 
 
-hx_ducts.height_array = ( ducts * hx_ducts.exh.height_array +  (ducts
-                            + 1) * hx_ducts.cool.height_array )
+hx_ducts.exh.height_array = np.zeros(hx_ducts.ducts.size)
+hx_ducts.cool.height_array = np.zeros(hx_ducts.ducts.size)
+hx_ducts.height_array = np.zeros(hx_ducts.ducts.size)
+
+# hx_ducts.exh.height_array = 3.5e-2 / hx_ducts.ducts
+# hx_ducts.cool.height_array = 2.e-2 / (hx_ducts.ducts + 1.)
+# hx_ducts.height_array = ( hx_ducts.ducts * hx_ducts.exh.height_array +  (hx_ducts.ducts + 1) * hx_ducts.cool.height_array )
+
+def get_height(height):
+    """solves for duct height such that total height is constrained"""
+    hx_ducts.exh.height = np.float(height)
+    hx_ducts.cool.height = 2. / 3.5 * hx_ducts.exh.height 
+
+    hx_ducts.height = ( (ducts + 1.) * (2. * hx_ducts.plate.thickness + hx_ducts.cool.height) + ducts * hx_ducts.exh.height )
+
+    total = 5.5 + 6. * hx_ducts.plate.thickness
+
+    error = hx_ducts.height - total
+
+    return error
+
+for i in range(hx_ducts.ducts.size):
+    ducts = hx_ducts.ducts[i]
+    fsolve(get_height, x0=0.01)
+    hx_ducts.exh.height_array[i] = hx_ducts.exh.height
+    hx_ducts.cool.height_array[i] = hx_ducts.cool.height
+    hx_ducts.height_array[i] = hx_ducts.height
+
 hx_ducts.cool.T_outlet_array = np.zeros(len(hx_ducts.height_array))
 hx_ducts.cool.T_inlet_array = np.zeros(len(hx_ducts.height_array))
 
 # Initializing arrays for storing loop results.  
-hx_ducts.Qdot_array = np.zeros(np.size(ducts))
-hx_ducts.tem.power_array = np.zeros(np.size(ducts)) 
-hx_ducts.power_net_array = np.zeros(np.size(ducts))
-hx_ducts.Wdot_pumping_array = np.zeros(np.size(ducts)) 
+hx_ducts.Qdot_array = np.zeros(hx_ducts.ducts.size)
+hx_ducts.tem.power_array = np.zeros(np.size(hx_ducts.ducts)) 
+hx_ducts.power_net_array = np.zeros(np.size(hx_ducts.ducts))
+hx_ducts.Wdot_pumping_array = np.zeros(np.size(hx_ducts.ducts)) 
 
 
-for i in np.arange(np.size(ducts)):
+for i in np.arange(np.size(hx_ducts.ducts)):
     hx_ducts.exh.height = hx_ducts.exh.height_array[i]
     hx_ducts.cool.height = hx_ducts.cool.height_array[i]
 
@@ -86,12 +109,12 @@ for i in np.arange(np.size(ducts)):
     hx_ducts.cool.T_outlet_array[i] = hx_ducts.cool.T_outlet
     hx_ducts.cool.T_inlet_array[i] = hx_ducts.cool.T_inlet
     
-    print "Finished solving for", ducts[i], "ducts\n"
+    print "Finished solving for", hx_ducts.ducts[i], "ducts\n"
     
-    hx_ducts.Qdot_array[i] = hx_ducts.Qdot * ducts[i]
-    hx_ducts.tem.power_array[i] = hx_ducts.tem.power_total * ducts[i]
-    hx_ducts.power_net_array[i] = hx_ducts.power_net * ducts[i]
-    hx_ducts.Wdot_pumping_array[i] = hx_ducts.Wdot_pumping * ducts[i]
+    hx_ducts.Qdot_array[i] = hx_ducts.Qdot * hx_ducts.ducts[i]
+    hx_ducts.tem.power_array[i] = hx_ducts.tem.power_total * hx_ducts.ducts[i]
+    hx_ducts.power_net_array[i] = hx_ducts.power_net * hx_ducts.ducts[i]
+    hx_ducts.Wdot_pumping_array[i] = hx_ducts.Wdot_pumping * hx_ducts.ducts[i]
     
 print "\nProgram finished."
 print "\nPlotting..."
@@ -108,18 +131,18 @@ plt.rcParams['lines.linewidth'] = 1.5
 FIGDIM1 = ([0.12, 0.12, 0.75, 0.75])
 
 XTICKS = hx_ducts.exh.height_array[0::3].copy() * 100.
-XTICKS = list(XTICKS)
+# XTICKS = list(XTICKS)
 
-for i in range(len(XTICKS)):
-    XTICKS[i] = ('{:01.1f}'.format(XTICKS[i])) 
+# for i in range(len(XTICKS)):
+#     XTICKS[i] = ('{:01.1f}'.format(XTICKS[i])) 
 
-XTICKS[0] = ''
+#XTICKS[0] = ''
 fig = plt.figure()
 ax1 = fig.add_axes(FIGDIM1)
-ax1.plot(ducts, hx_ducts.Qdot_array / 10., 'db', label=r'$\dot{Q}/10$') 
-ax1.plot(ducts, hx_ducts.tem.power_array, 'og', label='TEM')
-ax1.plot(ducts, hx_ducts.power_net_array, 'sr', label='$P_{net}$')  
-ax1.plot(ducts, hx_ducts.Wdot_pumping_array, '*k', label='Pumping')
+ax1.plot(hx_ducts.ducts, hx_ducts.Qdot_array / 10., 'db', label=r'$\dot{Q}/10$') 
+ax1.plot(hx_ducts.ducts, hx_ducts.tem.power_array, 'og', label='TEM')
+ax1.plot(hx_ducts.ducts, hx_ducts.power_net_array, 'sr', label='$P_{net}$')  
+ax1.plot(hx_ducts.ducts, hx_ducts.Wdot_pumping_array, '*k', label='Pumping')
 ax1.legend(loc='best')
 ax1.grid()
 ax1.set_xlabel('Ducts')
