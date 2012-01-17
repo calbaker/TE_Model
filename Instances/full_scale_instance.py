3f429fcb0fe8f56005bb94090aa2a8c429458f2a
Chad Baker <calbaker@gmail.com>
2012-01-16 18:26:32 -0600

    getting closer

--- Instances/full_scale_instance.py
+++ Instances/full_scale_instance.py
@@ -6,6 +6,8 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import os, sys
 from scipy.optimize import fsolve
+from scimath.units import * 
+from scimath.units.api import *
 
 # User Defined Modules
 
@@ -22,26 +24,26 @@ reload(hx)
 # 1.033370547666811676e-03
 # 4.634972529966798760e+00
 
-area = (0.01)**2
-length = 1.03e-3
-current = 4.63
+leg_area = UnitScalar((0.01)**2, units=length.m**2)
+leg_length = UnitScalar(1.03e-3, units=length.m)
+current = UnitScalar(4.63, units=SI.ampere)
 area_ratio = 0.705
 fill_fraction = 2.07e-2
 
 hx_inst = hx.HX()
 hx_inst.tem.method = 'analytical'
-hx_inst.width = 30.e-2
+hx_inst.width = UnitScalar(30.e-2, units=length.m)
 hx_inst.exh.bypass = 0.
-hx_inst.exh.height = 3.5e-2
-hx_inst.cool.mdot = 1.
-hx_inst.length = 1.
+hx_inst.exh.height = UnitScalar(3.5e-2, units=length.m)
+hx_inst.cool.mdot = UnitScalar(1., units=mass.kg / time.sec)
+hx_inst.length = UnitScalar(1., units=length.m)
 hx_inst.tem.I = current
-hx_inst.tem.length = length
+hx_inst.tem.length = leg_length
 
 hx_inst.tem.Ptype.material = 'HMS'
 hx_inst.tem.Ntype.material = 'MgSi'
 
-hx_inst.tem.Ptype.area = area                           
+hx_inst.tem.Ptype.area = leg_area                           
 hx_inst.tem.Ntype.area = hx_inst.tem.Ptype.area * area_ratio
 hx_inst.tem.area_void = ( (1. - fill_fraction) / fill_fraction *
                            (hx_inst.tem.Ptype.area +
@@ -49,10 +51,10 @@ hx_inst.tem.area_void = ( (1. - fill_fraction) / fill_fraction *
 
 hx_inst.type = 'counter'
 
-hx_inst.exh.T_inlet = 800.
-hx_inst.exh.P = 100.
-hx_inst.cool.T_inlet_set = 300.
-hx_inst.cool.T_outlet = 310.
+hx_inst.exh.T_inlet = UnitScalar(800., units=temperature.K)
+hx_inst.exh.P = UnitScalar(100., units=pressure.kPa)
+hx_inst.cool.T_inlet_set = UnitScalar(300., units=temperature.K)
+hx_inst.cool.T_outlet = UnitScalar(310., units=temperature.K)
 
 hx_inst.set_mdot_charge()
 hx_inst.cool.T_outlet = fsolve(hx_inst.get_T_inlet_error, x0=hx_inst.cool.T_outlet)
