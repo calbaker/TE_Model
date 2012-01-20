15ce4d62197efb8f4dddf761e7249b57010e5bd0
Chad Baker <calbaker@gmail.com>
2012-01-20 15:48:05 -0600

    turned on analytical

--- Instances/full_scale_instance.py
+++ Instances/full_scale_instance.py
@@ -29,7 +29,7 @@ area_ratio = 0.705
 fill_fraction = 2.07e-2
 
 hx_inst = hx.HX()
-# hx_inst.tem.method = 'analytical'
+hx_inst.tem.method = 'analytical'
 hx_inst.width = 30.e-2
 hx_inst.exh.bypass = 0.
 hx_inst.exh.height = 3.5e-2
