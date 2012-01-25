c677fd85380bc6052c5b1c3576406cd5011a4d6c
Chad Baker <calbaker@gmail.com>
2012-01-25 13:51:21 -0600

    should be ready to rock once I incorporate it into exhaust

--- Modules/jet_array.py
+++ Modules/jet_array.py
@@ -14,7 +14,8 @@ class JetArray(object):
         self.H : distance (m) from jet exit to impingement surface
         self.l : thickness (m) of the orifice plate
         self.K : minor loss coefficient. Fox, McDonald, and Pritchard
-        Table 8.2."""
+        Table 8.2.
+        self.spacing : distance (m) between adjacent jets"""
         
         self.D = 0.005
         self.H = 0.01 
@@ -41,7 +42,7 @@ class JetArray(object):
         self.N_transverse = width / self.spacing
         self.N = self.N_streamwise * self.N_transverse 
         
-    def set_flow(self, rho, Vdot):
+    def set_flow(self):
         """Determines pressure drop through jet array. 
 
         Sets the following variables
@@ -51,23 +52,44 @@ class JetArray(object):
         self.h_loss : head loss (m^2/s^2) through jet orifice
         self.deltaP : pressure drop (kPa) thorugh jet orifice
 
-        Arguments
+        Variables that must be set to run this method
         ------------
-        rho : density (kg/m^3) of fluid passing through jet
-        Vdot : volume flow rate (m^3/s) of fluid passing through jet
+        self.rho : density (kg/m^3) of fluid passing through jet
+        self.Vdot : volume flow rate (m^3/s) of fluid passing through jet
         array""" 
 
         self.area = np.pi * self.D**2 / 4. 
-        self.V = Vdot / (self.area * self.N) 
+        self.V = self.Vdot / (self.area * self.N) 
         self.h_loss = self.K * self.V**2 / 2.
-        self.deltaP = self.h_loss * rho * 0.001
+        self.deltaP = self.h_loss * self.rho * 0.001
 
-    def set_convection(self, nu, Pr):
-        """Variables that are set
+    def set_Nu_Dbar(self):
+        """Sets Nusselt number and some other variables
+
+        Sets the following variables
         -------------------------
         self.Nu_Dbar : average Nusselt number based on jet diameter
-        """
-        self.Re_D = self.V * self.D / self.nu
-        self.Nu_Dbar = ( 0.285 * self.Re_D**0.710 * Pr**0.33 * (self.H
-        / self.D)**-0.123 * (self.spacing / self.D)**-0.725 ) 
+        self.Re_D : Re based on jet diameter
         
+        Variables that must be set to run this method
+        ------------
+        self.nu : viscosity (m^2 / s) of fluid
+        self.Pr : Prandtl number of fluid"""
+
+        self.Re_D = self.V * self.D / self.nu
+        self.Nu_Dbar = ( 0.285 * self.Re_D**0.710 * self.Pr**0.33 *
+        (self.H / self.D)**-0.123 * (self.spacing / self.D)**-0.725 )  
+        
+    def solve_jet(self):
+        """Runs the following methods:
+        self.set_number
+        self.set_flow
+        self.set_Nu_Dbar"""
+        
+        self.set_number()
+        self.set_flow()
+        self.set_Nu_Dbar()
+        
+        
+        
+    
