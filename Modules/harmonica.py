6043a0775b335a7a252d62fdefe5412e1709aa98
Chad Baker <calbaker@gmail.com>
2012-01-26 15:59:57 -0600

    yep

--- Modules/harmonica.py
+++ Modules/harmonica.py
@@ -17,22 +17,34 @@ class Harmonica(object):
     the harmonica heat exchanger."""
     
     def __init__(self):
-    """Initiates hx1 and hx1, classes for dealing with the flow manifold
-    (upstream) heat exchanger and the working (downstream) heat
-    exchanger in which most of the heat transfer will take place.
+        """Initiates hx1 and hx1, classes for dealing with the flow manifold
+        (upstream) heat exchanger and the working (downstream) heat
+        exchanger in which most of the heat transfer will take place.
 
-    Sets
-    ------------------
-    self.hx2.exh.enhancement : see self.exh.__doc__
-    self.hx2.length : length (m) of side-flow hx
-    self.hx2.nodes : number of nodes in hx2"""
+        Sets
+        ------------------
+        self.height : height (m) of harmonica hx
+        self.length : overall length (m) of harmonica hx
+        self.hx2.exh.enhancement : see self.exh.__doc__
+        self.hx2.length : length (m) of side-flow hx
+        self.hx2.nodes : number of nodes in hx2"""
     
-    self.hx1 = hx.HX()
-    self.hx2 = hx.HX()
-    
-    self.hx2.exh.enhancement = "straight fins"
-    self.hx2.length = 0.05
-    self.hx2.nodes = 5
+        self.hx1 = hx.HX()
+        self.hx2 = hx.HX()
+
+        self.height = 1.e-2
+        self.length = 1. 
+        self.hx1.exh.T_inlet = 800.
+        self.hx1.cool.T_inlet = 300. 
+        self.hx2.cool.T_inlet = 300. 
+        
+        self.hx1.type = 'parallel'
+        self.hx2.type = 'parallel'
+
+        self.hx2.exh.enhancement = "straight fins"
+        self.hx2.length = 0.05
+        self.hx2.nodes = 5
+        
 
     def fix_geometry(self):
         """Makes sure that geometry for the two heat exchangers is the
@@ -42,16 +54,17 @@ class Harmonica(object):
         self.hx1.length = self.length
         self.hx2.width = self.hx1.length
         self.hx2.height = self.hx1.height
-        
+
     def solve_harmonica(self):
         """Solves both hx instances and sums the result."""
         
         self.fix_geometry()
+        self.hx1.set_mdot_charge()
         self.hx1.solve_hx()
 
         self.hx2.exh.T_inlet = self.hx1.exh.T_nodes.mean()
         self.hx2.exh.fins = 65
-        self.set_mdot_charge()
+        self.hx2.set_mdot_charge()
         self.hx2.solve_hx()
 
         self.Wdot_pumping = ( self.hx1.Wdot_pumping +
