3728819f2205af3333195cb09e236981dde776d8
Chad Baker <calbaker@gmail.com>
2012-01-31 11:15:06 -0600

    trying to use enhancement effectively

--- Modules/exhaust.py
+++ Modules/exhaust.py
@@ -10,10 +10,8 @@ reload(prop)
 # In this directory
 import functions
 reload(functions)
-import fin
-reload(fin)
-import jet_array
-reload(jet_array)
+import enhancement
+reload(enhancement)
 
 class Exhaust(prop.ideal_gas):
     """Class for modeling convection and flow of engine exhaust
@@ -38,10 +36,6 @@ class Exhaust(prop.ideal_gas):
         self.K : default permeability (m^2) of porous metal foam, used
         in Bejan model  
         self.Nu_coeff : coefficient used in Nusselt number calculation
-
-        self.fin : instance of fin.Fin class
-        self.jets : instance of jet_array.JetArray class 
-
         
         Binds the following methods from functions.py:
         set_flow_geometry
@@ -51,22 +45,16 @@ class Exhaust(prop.ideal_gas):
         Also initializes super class"""
 
         super(Exhaust, self).__init__()
-        self.porous = 'no' # is there porous media?
-        self.enhancement = 'none' # is there any means of enhancement? (i.e. fins,
-            # corrugate metal, etc.)
+
         self.T_ref = 300.
         self.P = 101.
         self.height = 1.5e-2
         self.ducts = 1
-        self.porosity = 0.92
-        self.k_matrix = 5.8e-3
-        self.PPI = 10.
-        self.K = 2.e-7
+
         self.Nu_coeff = 0.023
 
-        self.fin = fin.Fin() 
-        self.jets = jet_array.JetArray()
-
+        self.solve_enhancement = (
+        types.MethodType(enhancement.set_flow_geometry, self) )
         self.set_flow_geometry = (
         types.MethodType(functions.set_flow_geometry, self) )
         self.set_Re = (
@@ -82,119 +70,28 @@ class Exhaust(prop.ideal_gas):
         self.C = self.mdot * self.c_p # heat capacity of
         # flow (kW/K)
         self.Vdot = self.mdot / self.rho # volume flow rate (m^3/s) of exhaust
-        self.velocity = self.Vdot / self.area # velocity (m/s) of exhaust
+        self.velocity = self.Vdot / self.flow_area # velocity (m/s) of exhaust
 
-        if self.enhancement == 'Bejan porous':
-            self.Nu_D = 6. # Nu for porous media parallel plates with const
-                # heat flux.  Bejan Eq. 12.77 
-            self.Re_K = self.velocity * self.K**0.5 / self.nu # Re
-                # based on permeability from
-                # Bejan Eq. 12.11    
-            self.f = 1. / self.Re_K + 0.55 # Darcy Law, Bejan
-                # Eq. 12.14.  It turns out
-                # that f is pretty close to
-                # 0.55
-            self.k = self.k_matrix
-            self.deltaP = ( self.f * self.perimeter * self.length /
-            self.area * (0.5 * self.rho * self.velocity**2) * 0.001 ) 
-            # pressure drop (kPa) 
-            self.h = self.Nu_D * self.k / self.D 
-            # coefficient of convection (kW/m^2-K)
-
-        elif self.enhancement == 'Mancin porous':
-            self.k = self.k_matrix
-            self.Nu_D = 4.93 # Nu for porous media parallel plates with
-                # T_w = const.  Bejan Eq. 12.77
-            self.G = self.rho * self.velocity # Mass velocity from
-                # Mancin et al.
-            self.D_pore = 0.0122 * self.PPI**(-0.849) # hydraulic
-                # diameter (m?) of porous
-                # media based on Mancin et
-                # al.  
-            self.Re_K = ( self.D_pore * self.G / (self.mu * self.porosity) )
-            # Re of porous media from Mancin et al.
-            self.F = ( (1.765 * self.Re_K**(-0.1014) * self.porosity**2 /
-            self.PPI**(0.6)) ) # friction factor from Mancin et al. 
-            self.f = self.F 
-            # possibly wrong assignment but gets code to shut up and run 
-            self.deltaP = ( self.length * 2. * self.F * self.G**2 /
-            (self.D_pore * self.rho) * 0.001 ) 
-            # pressure drop from Mancin et al.
-            self.h = self.Nu_D * self.k / self.D 
-            # coefficient of convection (kW/m^2-K)
-
-        elif self.enhancement == 'straight fins':
-            # self.fins is the number of fins that fully extend across
-            # the duct.  The flow area and flow perimeter calculations
-            # depend on this.  For the heat transfer, this number also
-            # work. The fins will be spaced in such a way as to make
-            # an  array of square ducts. 
-            self.fin.height = self.height / 2
-            # height of fin pair such that their tips meet in the
-            # middle and are adiabatic.  
-            self.fin.length = self.length
-            self.fin.spacing = ( self.width - self.fins *
-        self.fin.thickness ) / (self.fins + 1)
-            self.flow_perimeter = ( 2. * ((self.width - self.fins *
-        self.fin.thickness) / (self.fins + 1.) + self.height) ) 
-            # perimeter of new duct formed by fins with constant overal duct width
-            self.flow_area = ( (self.width - self.fins *
-        self.fin.thickness) / (self.fins + 1.) * self.height ) 
-            # flow area (m^2) of new duct formed by fin    
-            self.D = 4. * self.flow_area / self.flow_perimeter
-            self.k = self.k_air
-            self.set_Re_dependents()
-            self.h = self.Nu_D * self.k / self.D
-            # coefficient of convection (kW/m^2-K) 
-            self.fin.h = self.h
-            self.fin.set_h()
-            self.h = ( (self.h * (self.width - self.fins *
-        self.fin.thickness) + self.fin.h_base * self.fins *
-        self.fin.thickness) / self.width ) 
-            self.deltaP = ( self.f * self.flow_perimeter * self.length
-        / self.flow_area * (0.5*self.rho * self.velocity**2) * 0.001 )  
-        # pressure drop (kPa)
-
-        elif self.enhancement == 'jet array':            
-            self.k = self.k_air
-            self.set_Re_dependents()
-            self.total_height = self.jets.H * 2. + self.height
-            self.jets.width = self.width
-            self.jets.length = self.length
-            self.jets.rho = self.rho
-            self.jets.Vdot = self.Vdot 
-            self.jets.nu = self.nu 
-            self.jets.Pr = self.Pr
-            self.jets.solve_jet()
-            
-            # for the following two calculations, the velocity is
-            # halved to consider the streamwise-averaged velocity for
-            # the pressure drop calculation
-            self.deltaP_duct = ( self.f * self.perimeter * self.length
-            / self.area * (0.5 * self.rho * (0.5 * self.velocity)**2) * 0.001 )  
-            # pressure drop (kPa)
-            self.deltaP_annulus = ( self.f * 2. *
-            self.jets.ann_perimeter * self.length / self.jets.ann_area
-            * (0.5 * self.rho * (0.5 * self.jets.ann_velocity)**2) * 0.001 ) 
-
-            self.deltaP = ( self.deltaP_duct + self.jets.deltaP +
-            self.deltaP_annulus ) 
-            self.h = self.jets.Nu_D * self.k / self.jets.D 
-            # coefficient of convection (kW/m^2-K)
-
-        elif self.enhancement == 'none':            
-            self.k = self.k_air
+        self.k = self.k_air
+        
+        try:
+            self.enhancement
+        except AttributeError:
+            self.enhancement = None
+        
+        if self.enhancement == None:
             self.set_Re_dependents()
             self.deltaP = ( self.f * self.perimeter * self.length /
-            self.area * (0.5*self.rho * self.velocity**2) * 0.001 ) 
+            self.flow_area * (0.5*self.rho * self.velocity**2) * 0.001 ) 
             # pressure drop (kPa)
             self.h = self.Nu_D * self.k / self.D 
             # coefficient of convection (kW/m^2-K)
+
+        else:
+            self.solve_enhancement()
 
         self.Wdot_pumping = self.Vdot * self.deltaP 
         # pumping power (kW)
-        if self.enhancement == 'straight fins':
-            self.fin.h = self.h
 
         self.R_thermal = 1. / self.h
         # thermal resistance (m^2-K/kW) of exhaust
