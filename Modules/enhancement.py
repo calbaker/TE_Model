746a4a4be045674fe085c2779c101ec0a171a358
Chad Baker <calbaker@gmail.com>
2012-02-01 15:59:12 -0600

    fixed up IdealFin

--- Modules/enhancement.py
+++ Modules/enhancement.py
@@ -67,9 +67,7 @@ class MancinPorous(object):
         # coefficient of convection (kW/m^2-K)
 
 class IdealFin(object):
-    """Class for modeling fin.  This class finds the necessary fin
-    parameters such that the efficiency is near unity, and therefore
-    the fin is isothermal."""
+    """Class for modeling fin."""
 
     def __init__(self):
         """Sets constants and things that need to be guessed to
@@ -83,17 +81,30 @@ class IdealFin(object):
         self.thickness = 5.e-3
         self.k = 0.2
 
-    def set_eta(self):
+    def set_eta(self,exh):
         """Determines fin efficiency"""
-        self.m = np.sqrt(2. * self.h / (self.k * self.thickness))
-        self.eta = ( np.tanh(self.m * self.height) / (self.m *
-        self.height) )
+        exh.set_Re_dependents()
+        exh.deltaP = ( exh.f * exh.perimeter * exh.length /
+                        exh.flow_area * (0.5*exh.rho * exh.velocity**2) * 0.001 ) 
+        # pressure drop (kPa)
+        exh.h = exh.Nu_D * exh.k / exh.D 
+        # coefficient of convection (kW/m^2-K)
+        
+        self.m = np.sqrt(2. * exh.h / (self.k * self.thickness))
+        self.eta = ( np.tanh(self.m * exh.height) / (self.m *
+        exh.height) )
 
-    def set_h(self):
+    def set_h(self,exh):
         """Determines effective heat transfer coefficient of fin."""
-        self.set_eta()
-        self.h_base = ( 2. * self.eta * self.h * self.height /
+        self.set_eta(exh)
+        self.h_base = ( 2. * self.eta * exh.h * exh.height /
         self.thickness )
+        exh.h_unfinned = exh.h
+        exh.h = self.h_base
+
+    def solve_enhancement(self,exh):
+        """Runs all the other methods that need to run."""
+        self.set_h(exh)
         
 class JetArray(object):
     """Class for modeling impinging jet array."""
