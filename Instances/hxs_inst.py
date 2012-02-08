3e63f6104b7ba0ddedd52598d2a68d652992dbde
Chad Baker <calbaker@gmail.com>
2012-02-08 14:12:10 -0600

    might be working

--- Instances/hxs_inst.py
+++ Instances/hxs_inst.py
@@ -12,8 +12,8 @@ cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
-import hx
-reload(hx)
+import hx_series 
+reload(hx_series)
     
 leg_area = (0.002)**2
 
@@ -22,33 +22,34 @@ fill_fraction = 2.13e-2
 leg_length = 1.10e-3
 current = 4.10
 
-hx_inst = hx.HX()
-hx_inst.te_pair.method = 'numerical'
-hx_inst.width = 30.e-2
-hx_inst.exh.height = 3.5e-2
-hx_inst.cool.mdot = 1.
-hx_inst.length = 1.
-hx_inst.te_pair.I = current
-hx_inst.te_pair.length = leg_length
+hxs = hx_series.HX_Series()
+hxs.te_pair.method = 'analytical'
+hxs.width = 30.e-2
+hxs.exh.height = 3.5e-2
+hxs.cool.mdot = 1.
+hxs.length = 1.
 
-hx_inst.te_pair.Ptype.material = 'HMS'
-hx_inst.te_pair.Ntype.material = 'MgSi'
+hxs.te_pair.I = current
+hxs.te_pair.length = leg_length
 
-hx_inst.te_pair.Ptype.area = leg_area                           
-hx_inst.te_pair.Ntype.area = hx_inst.te_pair.Ptype.area * area_ratio
-hx_inst.te_pair.area_void = ( (1. - fill_fraction) / fill_fraction *
-                           (hx_inst.te_pair.Ptype.area +
-                            hx_inst.te_pair.Ntype.area) )  
+hxs.te_pair.Ptype.material = 'HMS'
+hxs.te_pair.Ntype.material = 'MgSi'
 
-hx_inst.type = 'counter'
+hxs.te_pair.Ptype.area = leg_area                           
+hxs.te_pair.Ntype.area = hxs.te_pair.Ptype.area * area_ratio
+hxs.te_pair.area_void = ( (1. - fill_fraction) / fill_fraction *
+                           (hxs.te_pair.Ptype.area +
+                            hxs.te_pair.Ntype.area) )  
 
-hx_inst.exh.T_inlet = 800.
-hx_inst.cool.T_inlet_set = 300.
-hx_inst.cool.T_outlet = 310.
+hxs.type = 'parallel'
+hxs.setup()
 
-hx_inst.set_mdot_charge()
-# hx_inst.cool.T_outlet = fsolve(hx_inst.get_T_inlet_error, x0=hx_inst.cool.T_outlet)
-hx_inst.solve_hx()
+hxs.exh.T_inlet = 800.
+hxs.cool.T_inlet = 300.
+
+hxs.set_mdot_charge()
+# hxs.cool.T_outlet = fsolve(hxs.get_T_inlet_error, x0=hxs.cool.T_outlet)
+hxs.solve_hxs()
 
 print "\nProgram finished."
 print "\nPlotting..."
@@ -64,50 +65,9 @@ plt.rcParams['lines.linewidth'] = 1.5
 
 plt.close('all')
 
-plt.figure()
-plt.plot(hx_inst.x_dim * 100., hx_inst.exh.T_nodes, 'sr', label='Exhaust')
-plt.plot(hx_inst.x_dim * 100., hx_inst.te_pair.T_h_nodes, 'sg', label='TE_PAIR Hot Side')
-plt.plot(hx_inst.x_dim * 100., hx_inst.te_pair.T_c_nodes, 'sk', label='TE_PAIR Cold Side')
-plt.plot(hx_inst.x_dim * 100., hx_inst.cool.T_nodes, 'sb', label='Coolant')
-
-plt.xlabel('Distance Along HX (cm)')
-plt.ylabel('Temperature (K)')
-#plt.title('Temperature v. Distance, '+hx_inst.type)
-plt.grid()
-plt.legend(loc='best')
-plt.subplots_adjust(bottom=0.15)
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'temp.png')
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'temp.pdf')
-
-plt.figure()
-plt.plot(hx_inst.x_dim * 100., hx_inst.te_pair.power_nodes, 's', label='Exhaust')
-
-plt.xlabel('Distance Along HX (cm)')
-plt.ylabel('TEG Power (W)')
-plt.grid()
-plt.legend(loc='best')
-plt.subplots_adjust(bottom=0.15)
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'TEG power.png')
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'TEG power.pdf')
-
-plt.figure()
-plt.plot(hx_inst.x_dim * 100., hx_inst.exh.availability_flow_nodes, label='exhaust')
-plt.plot(hx_inst.x_dim * 100., hx_inst.cool.availability_flow_nodes, label='coolant')
-
-plt.xlabel('Distance Along HX (cm)')
-plt.ylabel('Availability (kW)')
-plt.grid()
-plt.legend(loc='best')
-plt.subplots_adjust(bottom=0.15)
-plt.subplots_adjust(left=0.18)
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'availability.png')
-plt.savefig('../Plots/' + hx_inst.te_pair.method + '/' + 'availability.pdf')
-
-# plt.show()
-
-print "power net:", hx_inst.power_net * 1000., 'W'
-print "power raw:", hx_inst.te_pair.power_total * 1000., 'W'
-print "pumping power:", hx_inst.Wdot_pumping * 1000., 'W'
-hx_inst.exh.volume = hx_inst.exh.height * hx_inst.exh.width * hx_inst.length
-print "exhaust volume:", hx_inst.exh.volume * 1000., 'L'
-print "exhaust power density:", hx_inst.power_net / hx_inst.exh.volume, 'kW/m^3'
+print "power net:", hxs.power_net * 1000., 'W'
+print "power raw:", hxs.te_pair.power_total * 1000., 'W'
+print "pumping power:", hxs.Wdot_pumping * 1000., 'W'
+hxs.exh.volume = hxs.exh.height * hxs.exh.width * hxs.length
+print "exhaust volume:", hxs.exh.volume * 1000., 'L'
+print "exhaust power density:", hxs.power_net / hxs.exh.volume, 'kW/m^3'
--- Modules/hx_series.py
+++ Modules/hx_series.py
@@ -9,7 +9,7 @@ import numpy as np
 import hx
 reload(hx)
 
-class hx_series(object):
+class HX_Series(hx.HX):
     """Class for modeling several heat exchanger in series that have
     different properties or parameters."""
 
@@ -18,4 +18,93 @@ class hx_series(object):
         ----------------------
         self.N : number of heat exchangers in series
         self."""
-        self.N = 
+        
+        self.N = 5
+
+        super(HX_Series, self).__init__()
+    
+    def setup(self):
+        """not sure what this should do yet.  It's an ad hoc method
+        for now.""" 
+        
+        self.hx_zone = list()
+
+        for i in range(self.N):
+            self.hx_zone.append(hx.HX())
+            self.hx_zone[i].type = self.type
+            self.hx_zone[i].length = self.length / self.N
+            self.hx_zone[i].width = self.width
+            self.hx_zone[i].exh.height = self.exh.height
+            self.hx_zone[i].cool.height = self.cool.height
+            self.hx_zone[i].te_pair.method = self.te_pair.method
+
+        self.nodes = self.N * self.nodes
+
+    def solve_hxs(self):
+        """ad hoc method for now."""
+        
+        self.hx_zone[0].exh.T_inlet = self.exh.T_inlet 
+        self.hx_zone[0].cool.T_inlet = self.cool.T_inlet
+        
+        self.hx_zone[0].exh.mdot = self.exh.mdot
+
+        self.hx_zone[0].optimize()
+        for i in range(1,self.N):
+            self.hx_zone[i].exh.mdot = self.exh.mdot
+            self.hx_zone[i].exh.T_inlet = self.hx_zone[0].exh.T_outlet  
+            self.hx_zone[i].cool.T_inlet = (
+        self.hx_zone[0].cool.T_outlet ) 
+            self.hx_zone[i].optimize()
+
+        self.store_zone_values()
+
+        # def store_zone_values(self):
+        #     """Concatenates all the zone values to make it look like a
+        #     single heat exchanger."""
+
+        #     self.power_net = np.array(hx_zone[:].power_net).sum()
+        #     self.te_pair.power_total = np.array(hx_zone[:]
+
+        
+        # self.Qdot_nodes = ZEROS.copy()
+        # # initialize array for storing heat transfer (kW) in each node 
+        
+        # self.exh.Vdot_nodes = ZEROS.copy()
+
+        # self.exh.T_nodes = ZEROS.copy()
+        # # initializing array for storing temperature (K) in each node 
+        # self.exh.h_nodes = ZEROS.copy()
+        # self.exh.f_nodes = ZEROS.copy()
+        # self.exh.deltaP_nodes = ZEROS.copy()
+        # self.exh.Wdot_nodes = ZEROS.copy()
+        # self.exh.Nu_nodes = ZEROS.copy()
+        # self.exh.c_p_nodes = ZEROS.copy()
+        # self.exh.entropy_nodes = ZEROS.copy()
+        # self.exh.enthalpy_nodes = ZEROS.copy()
+
+        # self.cool.T_nodes = ZEROS.copy()
+        # # initializing array for storing temperature (K) in each node 
+        # self.cool.entropy_nodes = ZEROS.copy()
+        # self.cool.enthalpy_nodes = ZEROS.copy()
+
+        # self.U_nodes = ZEROS.copy()
+        # self.U_hot_nodes = self.U_nodes.copy()
+        # self.U_cold_nodes = self.U_nodes.copy()
+        # self.q_h_nodes = ZEROS.copy()
+        # self.q_c_nodes = self.q_h_nodes.copy()
+        # self.te_pair.q_h_nodes = self.q_h_nodes.copy()
+        # self.te_pair.q_c_nodes = self.q_h_nodes.copy()
+
+        # self.error_hot_nodes = ZEROS.copy()
+        # self.error_cold_nodes = ZEROS.copy()
+
+        # self.te_pair.T_c_nodes = ZEROS.copy()
+        # # initializing array for storing temperature (K) in each node 
+        # self.te_pair.T_h_nodes = ZEROS.copy()
+        # # initializing array for storing temperature (K) in each node 
+        # self.te_pair.h_nodes = self.U_nodes.copy()
+
+        # self.te_pair.power_nodes = ZEROS.copy()
+        # self.te_pair.eta_nodes = ZEROS.copy()
+
+        # self.exh.velocity_nodes = ZEROS.copy()
