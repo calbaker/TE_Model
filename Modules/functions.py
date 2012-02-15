583ff95532a1dbee34d85efc1278f910b5404d67
Chad Baker <calbaker@gmail.com>
2012-02-15 16:34:08 -0600

    now set variable flow to 'laminar' or 'turbulent'

--- Modules/functions.py
+++ Modules/functions.py
@@ -21,20 +21,24 @@ def set_Re_dependents(self):
             # flow from Bejan
             self.Nu_D = self.Nu_coeff * self.Re_D**(4. / 5.) * self.Pr**(1. / 3.) # Adrian
             # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
+            self.flow = 'turbulent'
         else:
             self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
                 # parallel plates with constant T
             self.f = 24. / self.Re_D
+            self.flow = 'laminar'
     else:
         if (self.Re_D > 2300.): # Do these correlations hold for any tube geometry?
             self.f = 0.078 * self.Re_D**(-1. / 4.) # friction factor for turbulent
             # flow from Bejan
             self.Nu_D = self.Nu_coeff * self.Re_D**(4. / 5.)*self.Pr**(1. / 3.) # Adrian
             # Bejan, Convection Heat Transfer, 3rd ed., Equation 8.30
+            self.flow = 'turbulent'
         else:
             self.Nu_D = 7.54 # Bejan, Convection Heat Transfer, Table 3.2
                 # parallel plates with constant T
             self.f = 24. / self.Re_D
+            self.flow = 'laminar'
 
 def set_Re(self):
     """Sets
