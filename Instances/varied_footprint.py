b9ce4c23180321fdc0f3b05ee30ca6a50d4b9fe2
Chad Baker <calbaker@gmail.com>
2012-02-16 11:37:26 -0600

    runs, but output seems to be garbage

--- Instances/varied_footprint.py
+++ Instances/varied_footprint.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os,sys
-from scipy.optimize import fminbound, fmin
+from scipy.optimize import fsolve, fminbound, fmin
 
 # User Defined Modules
 cmd_folder = os.path.dirname('../Modules/')
@@ -16,22 +16,15 @@ reload(hx)
 
 leg_area = (0.002)**2
 
-area_ratio = 0.717
-fill_fraction = 2.98e-2
-leg_length = 3.50e-4
-current = 13.6
+area_ratio = 0.721
+fill_fraction = 3.13e-2
+leg_length = 3.45e-4
+current = 13.5
 
 hx1 = hx.HX()
-
-length = 1.
-width = 0.3
-
-hx1.length = length
-hx1.width = width
+hx1.width = 0.3
 hx1.exh.height = 3.5e-2
-
-hx1.footprint = hx1.length * hx1.width
-
+hx1.length = 1.
 hx1.te_pair.I = current
 hx1.te_pair.length = leg_length
 
@@ -44,17 +37,20 @@ hx1.te_pair.method = 'analytical'
 hx1.type = 'counter'
 hx1.exh.enh = hx1.exh.enh_lib.IdealFin()
 hx1.exh.enh.thickness = 1.e-3
+hx1.exh.enh.N = 60
 
 hx1.exh.T_inlet = 800.
 hx1.cool.T_inlet_set = 300.
 hx1.cool.T_outlet = 310.
 
 hx1.set_mdot_charge()
-
-def get_minpar(spacing):
+hx1.cool.T_outlet = fsolve(hx1.get_T_inlet_error,
+                                x0=hx1.cool.T_outlet)
+def get_minpar(apar):
     """Returns parameter to be minimized as a function of apar.
-    apar[0] : number of fins"""
-    hx1.exh.enh.spacing = spacing
+    apar : fin spacing"""
+    
+    hx1.exh.enh.spacing = apar
     hx1.solve_hx()
 
     if hx1.power_net < 0:
@@ -64,13 +60,14 @@ def get_minpar(spacing):
     
     return minpar
 
-# hx1.exh.enh.spacing = fminbound(get_minpar, 1.e-3, 10.e-3,
-# xtol=0.01)
-x0 = np.array([3.e-3])
-hx1.exh.enh.spacing = fmin(get_minpar, x0)
+x0 = 2.72e-3
+
+xmin = fmin(get_minpar, x0)
 
 N_fins = hx1.exh.enh.N
 print "Default number of fins:", N_fins
+
+hx1.footprint = hx1.width * hx1.length
 
 length_array = np.linspace(0.2, 2, 10)
 width_array = hx1.footprint / length_array 
@@ -92,8 +89,6 @@ for i in range(aspect_array.size):
     else:
         x0 = N_fins
 
-        # hx1.exh.enh.spacing = fminbound(get_minpar, 1.e-3, 15.e-3,
-        # xtol=0.01)
         hx1.exh.enh.spacing = fmin(get_minpar, x0=3.e-3, xtol=0.01)
 
     fin_array[i] = hx1.exh.enh.N
