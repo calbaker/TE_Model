dabd92c32c6a1abad81f92307a56f4ec954a1d6b
Chad Baker <calbaker@gmail.com>
2012-02-21 08:59:33 -0600

    first commit

---
+++ Instances/plot_TE_sensitivity.py
@@ -0,0 +1,45 @@
+# Chad Baker
+# Created on 2011 Feb 10
+
+# Distribution Modules
+import numpy as np
+import matplotlib.pyplot as plt
+import os,sys
+from scipy.optimize import fsolve
+from mayavi import mlab
+
+# User Defined Modules
+cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+    
+dirpath = '../data/TE_sensitivity/'
+current_array = np.load(dirpath+'current.npy')
+fill_array = np.load(dirpath+'fill_fraction.npy')
+leg_height_array = np.load(dirpath+'leg_height.npy') * 1.e3
+
+power_net_array = np.load(dirpath+'power_net.npy') 
+
+VMIN = 0.9 * power_net_array.max()
+
+mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(power_net_array),
+                            plane_orientation='x_axes',
+                            slice_index=current_array.size / 2,
+                                 vmin=VMIN
+                        )
+mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(power_net_array),
+                            plane_orientation='y_axes',
+                            slice_index=fill_array.size / 2,
+                                 vmin=VMIN
+                        )
+mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(power_net_array),
+                            plane_orientation='z_axes',
+                            slice_index=leg_height_array.size / 2,
+                                 vmin=VMIN
+                        )
+mlab.colorbar(title='Power (kW)')
+mlab.xlabel('Current (A)')
+mlab.ylabel('Fill Fraction (A)')
+mlab.zlabel('Leg Height (mm)')
+
+mlab.outline()
---
+++ Instances/plot_volume_varied.py
@@ -0,0 +1,55 @@
+# Chad Baker
+# Created on 2011 Feb 10
+
+# Distribution Modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+length_array = np.load('../data/volume_varied/length_array')
+width_array = np.load('../data/volume_varied/width_array')
+spacing_array = np.load('../data/volume_varied/spacing_array')
+power_net_array = np.load('../data/volume_varied/power_net_array')
+
+print "\nPlotting..."
+
+# Plot configuration
+FONTSIZE = 20
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+
+plt.close('all')
+
+plt.figure()
+x_2d, y_2d = np.meshgrid(length_array * 100., width_array * 100.)
+TICKS = np.linspace(800, power_net_array.max() * 1.e3, 12)
+LEVELS = np.linspace(800, power_net_array.max() * 1.e3, 12)
+FCS = plt.contourf(x_2d, y_2d, power_net_array.T * 1.e3, levels=LEVELS) 
+CB = plt.colorbar(FCS, orientation='vertical', format='%.0f', ticks=TICKS)
+CB.set_label(r'Power')
+plt.grid()
+plt.xlabel('Length (cm)')
+plt.ylabel('Width (cm)')
+plt.subplots_adjust(bottom=0.15)
+plt.subplots_adjust(left=0.15)
+plt.subplots_adjust(right=0.7)
+plt.savefig('../Plots/volume_varied/power_net.pdf')
+
+plt.figure()
+# TICKS = np.linspace(800, power_net_array.max() * 1.e3, 12)
+# LEVELS = np.linspace(800, power_net_array.max() * 1.e3, 12)
+FCS = plt.contourf(x_2d, y_2d, spacing_array.T * 1.e3)#, levels=LEVELS) 
+CB = plt.colorbar(FCS, orientation='vertical')#, format='%.0f', ticks=TICKS)
+CB.set_label(r'Fin Spacing (mm)')
+plt.grid()
+plt.xlabel('Length (cm)')
+plt.ylabel('Width (cm)')
+plt.subplots_adjust(bottom=0.15)
+plt.subplots_adjust(left=0.15)
+plt.subplots_adjust(right=0.7)
+plt.savefig('../Plots/volume_varied/fin spacing.pdf')
+
+# plt.show()
