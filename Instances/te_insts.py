e2cd25478463900c9b28f5672588b848dec7eca4
Chad Baker <calbaker@gmail.com>
2012-06-05 12:17:58 -0500

    pep8'd!

--- Instances/te_insts.py
+++ Instances/te_insts.py
@@ -1,7 +1,8 @@
 # distribution modules
-import numpy as np
 import matplotlib.pyplot as plt
-import time, os, sys
+import time
+import os
+import sys
 
 # local user modules
 cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
@@ -14,15 +15,15 @@ t0 = time.clock()
 
 length = 1.e-3
 current = 3.5
-area = (2.e-3)**2
-area_ratio = 0.69 
+area = (2.e-3) ** 2
+area_ratio = 0.69
 
 te_pair = te_pair.TE_Pair()
 # instantiate a te_pair object
 
 te_pair.Ntype.material = 'MgSi'
 te_pair.Ptype.material = 'HMS'
-# declare materials to be used for property calculations 
+# declare materials to be used for property calculations
 
 te_pair.I = current
 # set current to be used in both legs
@@ -36,13 +37,13 @@ te_pair.length = length
 #set leg length
 
 te_pair.set_constants()
-# Sets a bunch of attributes that are usually held constant. 
+# Sets a bunch of attributes that are usually held constant.
 
 te_pair.T_c_conv = 300.
-te_pair.T_h_conv = 700. 
+te_pair.T_h_conv = 700.
 
 te_pair.U_cold = 1.
-te_pair.U_hot = 0.5 
+te_pair.U_hot = 0.5
 
 te_pair.solve_te_pair()
 
@@ -55,7 +56,6 @@ plt.rcParams['xtick.labelsize'] = FONTSIZE
 plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 2.5
 plt.rcParams['lines.markersize'] = 10
-plt.rcParams['axes.formatter.limits'] = -3,3
+plt.rcParams['axes.formatter.limits'] = -3, 3
 
 plt.close('all')
-
