bfc397ca3f25fb362551ece92f40ec1e92db6e0b
Chad Baker <calbaker@gmail.com>
2012-07-17 16:42:31 -0500

    removing deprecated code that sucks.

--- Modules/transient.py
+++ Modules/transient.py
@@ -1,7 +1,13 @@
-# Chad Baker
-# Created on 2011 Feb 10
+"""
+Script defining transient HX class.
+
+Chad Baker
+Created on 2011 Feb 10
+"""
 
 # Distribution Modules
+import numpy as np
+import scipy.optimize as spopt
 
 # User Defined Modules
 # In this directory
@@ -10,17 +16,19 @@ reload(hx)
 import platewall
 reload(platewall)
 
+
 class Transient_HX(hx.HX):
     """Special class for modeling waste heat recovery heat exchanger
     with transient inlet temperature and flow conditions."""
 
     def __init__(self):
         """initializes variables."""
-        self.t_max = 5. # total elapsed time (s)
+        self.t_max = 5.  # total elapsed time (s)
         self.plate_hot = platewall.PlateWall()
+        self.t_step = 0.001  # time step (s)
         super(Transient_HX, self).__init__()
 
-    def get_error_hot_trans(self,T_h):
+    def get_error_hot_trans(self, T_h):
         """Needs better doc string."""
         self.plate_hot.solve_transient(self.exh.T, T_h)
         # hot side heat flux coming from plate into TE devices
@@ -45,41 +53,39 @@ class Transient_HX(hx.HX):
         After all the nodes have been iterated in this fashion, the
         time is incremented by the residence time of the exhaust in a
         single node (or perhaps less???)."""
-        
+
         self.init_arrays()
         self.solve_hx()
-        self.set_t_step()
         self.init_trans_zeros()
         self.init_trans_values()
 
-        for t in range(1,int(self.t_max / self.t_step)):
-	    if t%10==0:
-		print "t_index =", t, "of", int(self.t_max / self.t_step)
-	    self.exh.T = self.exh.T_inlet_trans[t]
-	    self.cool.T = self.cool.T_inlet
+        for t in range(1, int(self.t_max / self.t_step)):
+            if t % 10 == 0:
+                print "t_index =", t, "of", int(self.t_max / self.t_step)
+            self.exh.T = self.exh.T_inlet_trans[t]
+            self.cool.T = self.cool.T_inlet
             for i in range(self.nodes):
-                self.solve_node_transient(i,t)
-		self.Qdot_node = -self.tem.q_h * self.area
-                self.store_trans_values(i,t)
+                self.solve_node_transient(i, t)
+                self.Qdot_node = -self.tem.q_h * self.area
+                self.store_trans_values(i, t)
 
-		# redefining temperatures (K) for next node
-		self.exh.T = ( self.exh.T_trans[i-1,t-1]
-			       + self.plate_hot.q_h_trans[i-1,t-1]
-			       * self.area / self.exh.C )
+                # redefining temperatures (K) for next node
+                self.exh.T = (self.exh.T_trans[i - 1, t - 1]
+                               + self.plate_hot.q_h_trans[i - 1, t - 1]
+                               * self.area / self.exh.C)
 
-		if self.type == 'parallel':
-		    self.cool.T = ( self.cool.T_trans[i-1,t-1]
-				    - self.tem.q_c_trans[i-1,t-1]
-				    * self.area / self.cool.C )  
-		elif self.type == 'counter':
-		    self.cool.T = ( self.cool.T_trans[i-1,t-1]
-				    + self.tem.q_c_trans[i-1,t-1]
-				    * self.area / self.cool.C )  
+                if self.type == 'parallel':
+                    self.cool.T = (self.cool.T_trans[i - 1, t - 1]
+                                    - self.tem.q_c_trans[i - 1, t - 1]
+                                    * self.area / self.cool.C)
+                elif self.type == 'counter':
+                    self.cool.T = (self.cool.T_trans[i - 1, t - 1]
+                                    + self.tem.q_c_trans[i - 1, t - 1]
+                                    * self.area / self.cool.C)
 
-    def solve_node_transient(self,i,t):
+    def solve_node_transient(self, i, t):
         """needs a better doc string"""
 
-        
         # Determines current temperature in node i at time t based on
         # previous temperature in node i at time t-1, previous
         # temperature in node i-1 at time t-1, and what amount of
@@ -100,71 +106,69 @@ class Transient_HX(hx.HX):
         self.cool.T_node[i, t] = ((1. - self.exchange) * self.cool.T_node[i, t
         - 1] + self.exchange * self.cool.T_node[i - 1, t - 1])
 
-        self.tem.Ntype.node = i # used within tem.py
-        self.tem.Ptype.node = i
-        self.node = i
-        self.time = t
-        
-	self.tem.T_c = self.cool.T
-	# guess at cold side tem temperature (K)
-	self.tem.T_h_goal = self.exh.T
-	# guess at hot side TEM temperature (K)
-	self.tem.solve_te_pair()
-	self.set_convection()
-	self.q = self.U * (self.cool.T - self.exh.T)
-	self.tem.T_h_goal = self.q / self.U_hot + self.exh.T
-	self.tem.T_c = -self.q / self.U_cold + self.cool.T
+        self.tem.T_c = self.cool.T
+        # guess at cold side tem temperature (K)
+        self.tem.T_h_goal = self.exh.T
+        # guess at hot side TEM temperature (K)
+        self.tem.solve_te_pair()
+        self.set_convection()
+        self.q = self.U * (self.cool.T - self.exh.T)
+        self.tem.T_h_goal = self.q / self.U_hot + self.exh.T
+        self.tem.T_c = -self.q / self.U_cold + self.cool.T
 
-	self.plate_hot.T_prev = self.plate_hot.T_trans[:,i,t-1]
-        self.plate_hot.setup_transient(self.exh.h_trans[i,t-1])
-        self.error_hot = 100. # really big number to start while loop
+        self.plate_hot.T_prev = self.plate_hot.T_trans[:, i, t - 1]
+        self.plate_hot.setup_transient(self.exh.h_trans[i, t - 1])
 
-        self.loop_count = 0
-        while ( np.absolute(self.error_hot) > self.xtol or
-    np.absolute(self.error_cold) > self.xtol ): 
+        # This next block needs to be replaced with something that
+        # does not suck, but what???
 
-            self.tem.T_h_goal = spopt.fsolve(self.get_error_hot_trans,
-    x0=self.tem.T_h_goal)
-            # self.tem.solve_te_pair()
+    #     self.error_hot = 100.  # really big number to start while loop
+    #     self.loop_count = 0
 
-            self.tem.T_c = spopt.fsolve(self.get_error_cold,
-    x0=self.tem.T_c)
+    #     while (np.absolute(self.error_hot) > self.xtol or
+    # np.absolute(self.error_cold) > self.xtol):
 
-            self.tem.T_h_goal = spopt.fsolve(self.get_error_hot_trans,
-    x0=self.tem.T_h_goal)
-	    self.loop_count = self.loop_count + 1
-	    self.threshold = 10.
+    #         self.tem.T_h_goal = spopt.fsolve(self.get_error_hot_trans,
+    # x0=self.tem.T_h_goal)
+    #         # self.tem.solve_te_pair()
 
-	    if self.loop_count > self.threshold:
-		print ( "loop count is", self.loop_count,
-			" which exceeds threshold of", self.threshold )
+    #         self.tem.T_c = spopt.fsolve(self.get_error_cold,
+    # x0=self.tem.T_c)
 
-    def store_trans_values(self,i,t):
+    #         self.tem.T_h_goal = spopt.fsolve(self.get_error_hot_trans,
+    # x0=self.tem.T_h_goal)
+    #         self.loop_count = self.loop_count + 1
+    #         self.threshold = 10.
+
+    #         if self.loop_count > self.threshold:
+    #             print ("loop count is", self.loop_count,
+    #                     " which exceeds threshold of", self.threshold)
+
+    def store_trans_values(self, i, t):
         """Storing solved values in array to keep track of what
         happens in every node."""
-        self.Qdot_trans[i,t] = self.Qdot_node
+        self.Qdot_trans[i, t] = self.Qdot_node
         # storing node heat transfer in array
 
-        self.plate_hot.q_h_trans[i,t] = self.plate_hot.q_h
-        self.plate_hot.q_c_trans[i,t] = self.plate_hot.q_c
-        self.q_c_trans[i,t] = self.q_c
-        self.tem.q_h_trans[i,t] = self.tem.q_h
-        self.tem.q_c_trans[i,t] = self.tem.q_c
-        self.error_hot_trans[i,t] = self.error_hot
-        self.error_cold_trans[i,t] = self.error_cold
+        self.plate_hot.q_h_trans[i, t] = self.plate_hot.q_h
+        self.plate_hot.q_c_trans[i, t] = self.plate_hot.q_c
+        self.q_c_trans[i, t] = self.q_c
+        self.tem.q_h_trans[i, t] = self.tem.q_h
+        self.tem.q_c_trans[i, t] = self.tem.q_c
+        self.error_hot_trans[i, t] = self.error_hot
+        self.error_cold_trans[i, t] = self.error_cold
 
-        self.exh.T_trans[i,t] = self.exh.T
-        self.exh.h_trans[i,t] = self.exh.h
-        self.exh.f_trans[i,t] = self.exh.f
-        self.exh.Nu_trans[i,t] = self.exh.Nu_D
+        self.exh.T_trans[i, t] = self.exh.T
+        self.exh.h_trans[i, t] = self.exh.h
+        self.exh.f_trans[i, t] = self.exh.f
+        self.exh.Nu_trans[i, t] = self.exh.Nu_D
 
-        self.cool.h_trans[i,t] = self.cool.h
-        self.cool.T_trans[i,t] = self.cool.T
-        self.cool.f_trans[i,t] = self.cool.f
-        self.cool.Nu_trans[i,t] = self.cool.Nu_D
+        self.cool.h_trans[i, t] = self.cool.h
+        self.cool.T_trans[i, t] = self.cool.T
+        self.cool.f_trans[i, t] = self.cool.f
+        self.cool.Nu_trans[i, t] = self.cool.Nu_D
 
-        self.tem.T_h_trans[i,t] = self.tem.T_h_goal
-        # hot side temperature (K) of TEM at each node 
-        self.tem.T_c_trans[i,t] = self.tem.T_c
-        # cold side temperature (K) of TEM at each node.  
-
+        self.tem.T_h_trans[i, t] = self.tem.T_h_goal
+        # hot side temperature (K) of TEM at each node
+        self.tem.T_c_trans[i, t] = self.tem.T_c
+        # cold side temperature (K) of TEM at each node.
