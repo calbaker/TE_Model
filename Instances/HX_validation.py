cc38327b160d4da37edf15de4341adc8f6e5aa04
Chad Baker <calbaker@utexas.edu>
2012-08-02 13:11:26 -0500

    minor changes

--- Instances/HX_validation.py
+++ Instances/HX_validation.py
@@ -19,39 +19,39 @@ fill_fraction = 3.10e-2
 leg_length = 3.56e-4
 current = 13.0
 
-hx_fins0 = hx.HX()
+hx_exp = hx.HX()
 
-hx_fins0.width = 0.55
-hx_fins0.exh.height = 3.5e-2
-hx_fins0.length = 0.55
-hx_fins0.te_pair.I = current
-hx_fins0.te_pair.length = leg_length
-hx_fins0.te_pair.leg_area_ratio = area_ratio
-hx_fins0.te_pair.fill_fraction = fill_fraction
+hx_exp.width = 0.55
+hx_exp.exh.height = 3.5e-2
+hx_exp.length = 0.55
+hx_exp.te_pair.I = current
+hx_exp.te_pair.length = leg_length
+hx_exp.te_pair.leg_area_ratio = area_ratio
+hx_exp.te_pair.fill_fraction = fill_fraction
 
-hx_fins0.te_pair.set_leg_areas()
+hx_exp.te_pair.set_leg_areas()
 
-hx_fins0.te_pair.Ntype.material = 'MgSi'
-hx_fins0.te_pair.Ptype.material = 'HMS'
+hx_exp.te_pair.Ntype.material = 'MgSi'
+hx_exp.te_pair.Ptype.material = 'HMS'
 
-hx_fins0.type = 'counter'
+hx_exp.type = 'counter'
 
-hx_fins0.exh.enh = hx_fins0.exh.enh_lib.IdealFin()
-hx_fins0.exh.enh.thickness = 1.e-3
-hx_fins0.exh.enh.spacing = 1.26e-3
+hx_exp.exh.enh = hx_exp.exh.enh_lib.IdealFin()
+hx_exp.exh.enh.thickness = 1.e-3
+hx_exp.exh.enh.spacing = 1.26e-3
 
-hx_fins0.cool.enh = hx_fins0.cool.enh_lib.IdealFin()
-hx_fins0.cool.enh.thickness = 1.e-3
-hx_fins0.cool.enh.spacing = 1.e-3
+hx_exp.cool.enh = hx_exp.cool.enh_lib.IdealFin()
+hx_exp.cool.enh.thickness = 1.e-3
+hx_exp.cool.enh.spacing = 1.e-3
 
-hx_fins0.exh.T_inlet = 800.
-hx_fins0.cool.T_inlet_set = 300.
-hx_fins0.cool.T_outlet = 310.
+hx_exp.exh.T_inlet = 800.
+hx_exp.cool.T_inlet_set = 300.
+hx_exp.cool.T_outlet = 310.
 
-hx_fins0.set_mdot_charge()
-# hx_fins0.cool.T_outlet = fsolve(hx_fins0.get_T_inlet_error,
-#                                 x0=hx_fins0.cool.T_outlet, xtol=0.01)
-hx_fins0.solve_hx()
+hx_exp.set_mdot_charge()
+# hx_exp.cool.T_outlet = fsolve(hx_exp.get_T_inlet_error,
+#                                 x0=hx_exp.cool.T_outlet, xtol=0.01)
+hx_exp.solve_hx()
 
 print "\nProgram finished."
 print "\nPlotting..."
@@ -66,14 +66,14 @@ plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 
 plt.figure()
-plt.plot(hx_fins0.x * 100., hx_fins0.exh.T_nodes, '-r', label='Exhaust')
-plt.plot(hx_fins0.x * 100., hx_fins0.te_pair.T_h_nodes, '-g', label='TE Hot Side')
-plt.plot(hx_fins0.x * 100., hx_fins0.te_pair.T_c_nodes, '-k', label='TE Cold Side')
-plt.plot(hx_fins0.x * 100., hx_fins0.cool.T_nodes, '-b', label='Coolant')
+plt.plot(hx_exp.x * 100., hx_exp.exh.T_nodes, '-r', label='Exhaust')
+plt.plot(hx_exp.x * 100., hx_exp.te_pair.T_h_nodes, '-g', label='TE Hot Side')
+plt.plot(hx_exp.x * 100., hx_exp.te_pair.T_c_nodes, '-k', label='TE Cold Side')
+plt.plot(hx_exp.x * 100., hx_exp.cool.T_nodes, '-b', label='Coolant')
 
 plt.xlabel('Distance Along HX (cm)')
 plt.ylabel('Temperature (K)')
-#plt.title('Temperature v. Distance, '+hx_fins0.type)
+#plt.title('Temperature v. Distance, '+hx_exp.type)
 plt.grid()
 # plt.legend(loc='center left')
 plt.subplots_adjust(bottom=0.15)
@@ -84,11 +84,11 @@ plt.savefig('../Plots/fin_inst/temp.pdf')
 
 # plt.show()
 
-print "power net:", hx_fins0.power_net * 1000., 'W'
-print "power raw:", hx_fins0.te_pair.power_total * 1000., 'W'
-print "pumping power:", hx_fins0.Wdot_pumping * 1000., 'W'
-hx_fins0.exh.volume = hx_fins0.exh.height * hx_fins0.exh.width * hx_fins0.length
-print "exhaust volume:", hx_fins0.exh.volume * 1000., 'L'
-print "exhaust power density:", hx_fins0.power_net / hx_fins0.exh.volume, 'kW/m^3'
+print "power net:", hx_exp.power_net * 1000., 'W'
+print "power raw:", hx_exp.te_pair.power_total * 1000., 'W'
+print "pumping power:", hx_exp.Wdot_pumping * 1000., 'W'
+hx_exp.exh.volume = hx_exp.exh.height * hx_exp.exh.width * hx_exp.length
+print "exhaust volume:", hx_exp.exh.volume * 1000., 'L'
+print "exhaust power density:", hx_exp.power_net / hx_exp.exh.volume, 'kW/m^3'
 
 
