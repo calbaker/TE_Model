cb41bb1d73b6b6d3a5d036945f68bd2fd4d6c00e
Chad Baker <calbaker@utexas.edu>
2012-08-02 13:22:42 -0500

    hx_validation is broken, and this is just from changing settings

--- Instances/hx_validation.py
+++ Instances/hx_validation.py
@@ -14,10 +14,13 @@ reload(hx)
 
 hx_exp = hx.HX()
 
-hx_exp.width = 0.55
-hx_exp.exh.height = 3.5e-2
-hx_exp.length = 0.55
+# These values must be checked
+hx_exp.width = 20. * 2.54e-2
+hx_exp.exh.height = 2.5 * 2.54e-2
+hx_exp.cool.height = 1. * 2.54e-2
+hx_exp.length = 20. * 2.54e-2
 
+hx_exp.te_pair.Ptype.area = (0.02e-3) ** 2
 hx_exp.te_pair.I = 13.0
 hx_exp.te_pair.length = 3.56e-4
 hx_exp.te_pair.leg_area_ratio = 0.745
