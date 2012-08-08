d85e1408c191173d11b9df96fd19f10c54c94f66
Chad Baker <calbaker@utexas.edu>
2012-08-08 13:18:24 -0500

    changes that do not matter at all

--- Instances/ad_hoc.py
+++ Instances/ad_hoc.py
@@ -1,14 +1,9 @@
-"""This script runs the model for designs available from
-heat sink usa""" 
-
 # Chad Baker
 # Created on 2011 Feb 10
 
 # Distribution Modules
-import numpy as np
 import matplotlib.pyplot as plt
-import os,sys
-from scipy.optimize import fsolve, fmin
+import os, sys
 
 # User Defined Modules
 cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
@@ -17,73 +12,41 @@ if cmd_folder not in sys.path:
 import hx
 reload(hx)
 
-leg_area = (0.002)**2
+hx_ad_hoc = hx.HX()
 
-area_ratio = 0.849
-fill_fraction = 3.85e-2
-leg_length = 4.00e-4
-current = 13.2
+# These values must be checked
+hx_ad_hoc.width = 20. * 2.54e-2
+hx_ad_hoc.exh.height = 2.5 * 2.54e-2
+hx_ad_hoc.cool.height = 1. * 2.54e-2
+hx_ad_hoc.length = 20. * 2.54e-2
 
-hx0 = hx.HX()
+hx_ad_hoc.te_pair.Ptype.area = (2.e-3) ** 2
 
-hx0.width       = (20. - 0.640) * 2.54e-2
-hx0.exh.height  = (2.5 - 0.375) * 2.54e-2
-hx0.cool.height = 1.25 * 2.54e-2
-hx0.length      = 20. * 2.54e-2
+hx_ad_hoc.te_pair.leg_area_ratio = 0.662
+hx_ad_hoc.te_pair.I = 0.001  # turns off TE effect
+hx_ad_hoc.te_pair.length = 1.e-5
+hx_ad_hoc.te_pair.fill_fraction = 1.
 
-hx0.plate.thickness = 0.375 * 2.54e-2
+hx_ad_hoc.te_pair.set_leg_areas()
 
-hx0.te_pair.I = current
-hx0.te_pair.length = leg_length
+hx_ad_hoc.te_pair.Ntype.material = 'MgSi'
+hx_ad_hoc.te_pair.Ptype.material = 'HMS'
 
-hx0.te_pair.Ntype.material = 'MgSi'
-hx0.te_pair.Ptype.material = 'HMS'
+hx_ad_hoc.type = 'counter'
 
-hx0.te_pair.set_all_areas(leg_area, area_ratio, fill_fraction)
+hx_ad_hoc.exh.T_inlet = 406.1 + 273.15
+hx_ad_hoc.cool.T_inlet_set = 300.
+hx_ad_hoc.cool.T_outlet = 310.
 
-hx0.te_pair.method = 'numerical'
-hx0.type = 'counter'
+hx_ad_hoc.cummins.RPM = 1940.
+hx_ad_hoc.set_mdot_charge()
 
-thickness = 0.080 * 2.54e-2
-spacing = (0.400 - 0.080) * 2.54e-2
+# hx_ad_hoc.cool.T_outlet = fsolve(hx_ad_hoc.get_T_inlet_error,
+#                                 x0=hx_ad_hoc.cool.T_outlet, xtol=0.01)
+hx_ad_hoc.solve_hx()
 
-hx0.exh.enh = hx0.exh.enh_lib.IdealFin()
-hx0.exh.enh.thickness = thickness
-hx0.exh.enh.spacing = (0.200 - 0.080) * 2.54e-2
-hx0.exh.enh.l = 3. / 8. * 2.54e-2
-
-hx0.cool.enh = hx0.cool.enh_lib.IdealFin()
-hx0.cool.enh.thickness = thickness
-hx0.cool.enh.spacing = (0.400 - 0.080) * 2.54e-2 
-
-# hx0.apar_list.append(['self','exh','enh','l'])
-# hx0.apar_list.append(['self','length'])
-
-hx0.exh.T_inlet = 800.
-hx0.cool.T_inlet_set = 300.
-hx0.cool.T_outlet = 307.7
-
-hx0.set_mdot_charge()
-
-hx0.solve_hx()
-
-power0 = hx0.power_net
-
-nodes_array = np.arange(20,212,1)
-power = np.zeros(nodes_array.size) 
-
-for i in range(nodes_array.size):
-    print "\nSolving", i+1, "of", nodes_array.size 
-    hx0.nodes = nodes_array[i]
-    hx0.solve_hx()
-    power[i] = hx0.power_net
-
-    print hx0.power_net
-
-power_norm = power / power0 
-power_diff = (power - power0) / power0 * 100.
-
-print "\nPreparing plots.\n"
+print "\nProgram finished."
+print "\nPlotting..."
 
 # Plot configuration
 FONTSIZE = 20
@@ -94,19 +57,31 @@ plt.rcParams['xtick.labelsize'] = FONTSIZE
 plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 
-plt.close()
-
 plt.figure()
-plt.plot(nodes_array, power_diff)
+plt.plot(hx_ad_hoc.x * 100., hx_ad_hoc.exh.T_nodes, '-r', label='Exhaust')
+plt.plot(hx_ad_hoc.x * 100., hx_ad_hoc.te_pair.T_h_nodes, '-g', label='TE Hot Side')
+plt.plot(hx_ad_hoc.x * 100., hx_ad_hoc.te_pair.T_c_nodes, '-k', label='TE Cold Side')
+plt.plot(hx_ad_hoc.x * 100., hx_ad_hoc.cool.T_nodes, '-b', label='Coolant')
 
-plt.xlabel('Number of Streamwise Nodes')
-plt.ylabel('Change (%) in Power\nCompared to 25 Nodes')
+plt.xlabel('Distance Along HX (cm)')
+plt.ylabel('Temperature (K)')
+#plt.title('Temperature v. Distance, '+hx_ad_hoc.type)
 plt.grid()
-plt.subplots_adjust(bottom=0.12)
-plt.subplots_adjust(left=0.20)
+# plt.legend(loc='center left')
+plt.subplots_adjust(bottom=0.15)
+plt.ylim(ymax=950)
+plt.legend(loc="upper right")
+plt.savefig('../Plots/fin_inst/temp.png')
+plt.savefig('../Plots/fin_inst/temp.pdf')
 
-plt.savefig("../Plots/ad_hoc/streamwise.pdf")
-plt.savefig("../Plots/ad_hoc/streamwise.png")
+# plt.show()
 
-plt.show()
+print "hot side heat transfer:", hx_ad_hoc.Qdot_total, 'W'
+print "power net:", hx_ad_hoc.power_net * 1000., 'W'
+print "power raw:", hx_ad_hoc.te_pair.power_total * 1000., 'W'
+print "pumping power:", hx_ad_hoc.Wdot_pumping * 1000., 'W'
+hx_ad_hoc.exh.volume = hx_ad_hoc.exh.height * hx_ad_hoc.exh.width * hx_ad_hoc.length
+print "exhaust volume:", hx_ad_hoc.exh.volume * 1000., 'L'
+print "exhaust power density:", hx_ad_hoc.power_net / hx_ad_hoc.exh.volume, 'kW/m^3'
+
 
