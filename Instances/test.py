a95635f51f90e4727e2c22aebd388784d5fa2e7a
Chad Baker <calbaker@utexas.edu>
2012-09-05 14:59:28 -0500

    leg_instance was renamed to leg_inst

--- Instances/test.py
+++ Instances/test.py
@@ -5,6 +5,6 @@ caused by these changes."""
 execfile('te_inst.py')
 execfile('fin_inst.py')
 execfile('osf_inst.py')
-execfile('leg_instance.py')
+execfile('leg_inst.py')
 execfile('hx_validation.py')
 execfile('hx_validation_noTE.py')
