694a488c55077d3a0d96c1812dc29acba098c520
Chad Baker <calbaker@utexas.edu>
2012-09-05 14:57:10 -0500

    used for making sure deprecated stuff is actually deprecated before removing it

---
+++ Instances/test.py
@@ -0,0 +1,10 @@
+"""This script is used to run several other scripts that may fail due
+to changes in Modules.  This script will help troubleshoot failures
+caused by these changes."""
+
+execfile('te_inst.py')
+execfile('fin_inst.py')
+execfile('osf_inst.py')
+execfile('leg_instance.py')
+execfile('hx_validation.py')
+execfile('hx_validation_noTE.py')
