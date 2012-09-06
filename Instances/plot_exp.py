363d8667308319961acc544a4100e3ffe81dc461
Chad Baker <calbaker@utexas.edu>
2012-09-06 17:23:50 -0500

    kinda works, but effects of mdot and t_in need to be checked

--- Instances/plot_exp.py
+++ Instances/plot_exp.py
@@ -1,5 +1,7 @@
 """Plots results from heat exchanger experiments."""
 
+from mpl_toolkits.mplot3d import Axes3D
+# pychecker will flag this statement, but it is necessary
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -20,7 +22,12 @@ exp_data.file = '2012-09-04 gypsum.csv'
 exp_data.import_data()
 
 mod_data = real_hx.get_hx()
-
+k_gypsum = 0.17e-3
+# thermal conductivity (kW / (m * K)) of gypsum board from Incropera
+# and DeWitt Intro. to Heat Transfer 5th ed., Table A.3
+thickness_gypsum = 0.25 * 2.54e-2
+# thickness (m) of gypsum board
+mod_data.R_extra = thickness_gypsum / k_gypsum
 mod_data.Qdot_arr = np.zeros(exp_data.exh.T_in.size)
 
 for i in range(exp_data.exh.T_in.size):
@@ -34,7 +41,7 @@ for i in range(exp_data.exh.T_in.size):
     mod_data.Qdot_arr[i] = mod_data.Qdot_total
 
 # Plot configuration
-FONTSIZE = 18
+FONTSIZE = 12
 plt.rcParams['axes.labelsize'] = FONTSIZE
 plt.rcParams['axes.titlesize'] = FONTSIZE
 plt.rcParams['legend.fontsize'] = FONTSIZE
@@ -43,26 +50,7 @@ plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 
-TICKS = exp_data.exh.Qdot.copy()
-TICKS.sort()
-LEVELS = TICKS
-
 plt.close()
-
-# fig1 = plt.figure()
-# FCS = plt.contourf(
-#     exp_data.exh.T_in.reshape(2, 2), exp_data.exh.mdot.reshape(2, 2),
-#     exp_data.exh.Qdot.reshape(2, 2), levels=LEVELS
-#     )
-# CB = plt.colorbar(
-#     FCS, orientation='vertical', format='%.2f', ticks=TICKS
-#     )
-# CB.set_label(r'$\dot{Q}$ (kW)')
-# plt.grid()
-# plt.xlabel('Charge Flow (kg/s)')
-# plt.ylabel('Exh Inlet Temp (K)')
-
-# plt.savefig('Plots/plot_exp/Qdot.pdf')
 
 dx = (exp_data.exh.mdot.max() - exp_data.exh.mdot.min()) / 10.
 dy = (exp_data.exh.T_in.max() - exp_data.exh.T_in.min()) / 10.
@@ -75,7 +63,7 @@ ax.bar3d(
     )
 ax.bar3d(
     exp_data.exh.mdot, exp_data.exh.T_in,
-    np.zeros(exp_data.exh.Qdot.size), dx * 0.5, dy * 0.5,
+    np.zeros(exp_data.exh.Qdot.size), -dx, -dy,
     mod_data.Qdot_arr, color='r'
     )
 
