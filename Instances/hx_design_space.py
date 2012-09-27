82967072fe3602748513eda9c85bc1b6b30e7815
Chad Baker <calbaker@utexas.edu>
2012-09-27 14:18:24 -0500

    first commit in this form

---
+++ Instances/hx_design_space.py
@@ -0,0 +1,113 @@
+# Chad Baker
+# Created on 2011 Feb 10
+
+# Distribution Modules
+import numpy as np
+import os
+import sys
+
+# User Defined Modules
+cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+import te_pair
+reload(te_pair)
+
+area = (0.002) ** 2.
+te_design = te_pair.TE_Pair()
+te_design.Ptype.area = area
+te_design.leg_area_ratio = 0.693
+te_design.fill_fraction = 0.796
+te_design.length = 1.45e-3
+te_design.I = 1.87
+te_design.Ntype.material = 'MgSi'
+te_design.Ptype.material = 'HMS'
+
+te_design.set_leg_areas()
+
+te_design.T_c_conv = 300.  # cold side convection temperature (K)
+te_design.T_h_conv = 600.  # hot side convection temperature (K)
+
+te_design.U_cold = 8.
+# cold side overall heat transfer coeffcient (kW / (m ** 2 * K))
+te_design.U_hot = 2.
+# hot side overall heat transfer coeffcient (kW / (m ** 2 * K))
+
+te_design.optimize()
+leg_area_ratio = te_design.leg_area_ratio
+fill_fraction = te_design.fill_fraction
+length = te_design.length
+current = te_design.I
+
+SIZE = 50
+current_array = np.linspace(0.5, 2, SIZE) * te_design.I
+fill_array = (
+    np.linspace(0.5, 2, current_array.size + 1) *
+    te_design.fill_fraction
+    )
+length_array = (
+    np.linspace(0.5, 2, fill_array.size + 1) * te_design.length
+    )
+
+power_I_fill = np.zeros(
+    [current_array.size, fill_array.size]
+    )
+power_fill_height = np.zeros(
+    [fill_array.size, length_array.size]
+    )
+power_height_I = np.zeros(
+    [length_array.size, current_array.size]
+    )
+
+for i in range(current_array.size):
+    te_design.I = current_array[i]
+    print "i =", i
+    for j in range(fill_array.size):
+        te_design.fill_fraction = fill_array[j]
+        te_design.set_leg_areas()
+        te_design.solve_te_pair()
+        power_I_fill[i, j] = te_design.P_flux
+
+te_design.I = current
+te_design.fill_fraction = fill_fraction
+te_design.length = length
+
+for j in range(fill_array.size):
+    te_design.fill_fraction = fill_array[j]
+    te_design.set_leg_areas()
+    print "j =", j
+    for k in range(length_array.size):
+        te_design.length = length_array[k]
+        te_design.solve_te_pair()
+        power_fill_height[j, k] = te_design.P_flux
+
+te_design.I = current
+te_design.fill_fraction = fill_fraction
+te_design.length = length
+te_design.set_leg_areas()
+
+for k in range(length_array.size):
+    te_design.length = length_array[k]
+    te_design.solve_te_pair()
+    print "k =", k
+    for i in range(current_array.size):
+        te_design.I = current_array[i]
+        te_design.solve_te_pair()
+        power_height_I[k, i] = te_design.P_flux
+
+te_design.I = current
+te_design.fill_fraction = fill_fraction
+te_design.length = length
+
+data_dir = '../output/te_design_space/'
+np.save(data_dir + 'power_I_fill', power_I_fill)
+np.save(data_dir + 'power_fill_height', power_fill_height)
+np.save(data_dir + 'power_height_I', power_height_I)
+np.save(data_dir + 'current_array', current_array)
+np.save(data_dir + 'fill_array', fill_array)
+np.save(data_dir + 'length_array', length_array)
+
+print "\nProgram finished."
+print "\nPlotting..."
+
+execfile('plot_TE_sensitivity.py')
---
+++ Instances/plot_hx_design_space.py
@@ -0,0 +1,113 @@
+# Chad Baker
+# Created on 2011 Feb 10
+
+# Distribution Modules
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import os
+import sys
+
+# User Defined Modules
+cmd_folder = os.path.dirname(os.path.abspath('../Modules/hx.py'))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+    
+data_dir = '../output/te_design_space/'
+current_array = np.load(data_dir + 'current_array.npy') 
+fill_array = np.load(data_dir + 'fill_array.npy')
+length_array = np.load(data_dir + 'length_array.npy')
+power_I_fill = np.load(data_dir + 'power_I_fill.npy')
+power_fill_height = np.load(data_dir + 'power_fill_height.npy')
+power_height_I = np.load(data_dir + 'power_height_I.npy')
+
+# Plot configuration
+FONTSIZE = 18
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+
+plt.close()
+save_dir = "../Plots/plot_TE_sensitivity/"
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+
+X1, Y1 = np.meshgrid(current_array, fill_array * 1e2)
+Z1 = power_I_fill.T
+# cset = ax.contourf(X1, Y1, Z1, zdir='z', offset=length_array[0])
+
+X2, Y2 = np.meshgrid(fill_array * 1e2, length_array * 1e3)
+Z2 = power_fill_height.T
+# cset = ax.contourf(X2, Y2, Z2, zdir='x', offset=current_array[0])
+
+X3, Y3 = np.meshgrid(length_array * 1e3, current_array)
+Z3 = power_height_I.T
+# cset = ax.contourf(X3, Y3, Z3, zdir='x', offset=fill_array[-1])
+
+# ax.set_xlabel('Current (A)')
+# ax.set_xlim(current_array[0], current_array[-1])
+# ax.set_ylabel('Fill Fraction')
+# ax.set_ylim(fill_array[0], fill_array[-1])
+# ax.set_zlabel('Leg Height (mm)')
+# ax.set_zlim(length_array[0], length_array[-1])
+
+RIGHT = 0.75
+BOTTOM = 0.17
+FRACTION = 0.15
+MAX_ARRAY = np.array([Z1.max(), Z2.max(), Z3.max()])
+MAX = MAX_ARRAY.max()
+LEVELS = (
+    (MAX + 0.1 - np.logspace(np.log10(0.1), np.log10(MAX), 12))[::-1]
+    )
+TICKS = LEVELS
+FORMAT = '%0.1f'
+
+fig1 = plt.figure('Z1')
+cset1 = plt.contourf(X1, Y1, Z1, levels=LEVELS)
+CB1 = plt.colorbar(
+    cset1, orientation='vertical', format=FORMAT, fraction=FRACTION,
+    ticks=TICKS
+    )
+CB1.set_label(r'Power Flux (kW/m$^2$)')
+plt.xticks(rotation=40)
+plt.xlabel('Current (A)')
+plt.ylabel('Fill Fraction (%)')
+plt.subplots_adjust(bottom=BOTTOM, right=RIGHT)
+plt.grid()
+fig1.savefig(save_dir + "power_I_fill.pdf")
+
+fig2 = plt.figure('Z2')
+cset2 = plt.contourf(X2, Y2, Z2, levels=LEVELS)
+CB2 = plt.colorbar(
+    cset2, orientation='vertical', format=FORMAT, fraction=FRACTION,
+    ticks=TICKS
+    )
+CB2.set_label(r'Power Flux (kW/m$^2$)')
+plt.xticks(rotation=40)
+plt.xlabel('Fill Fraction (%)')
+plt.ylabel('Length (mm)')
+plt.subplots_adjust(bottom=BOTTOM, right=RIGHT)
+plt.grid()
+fig2.savefig(save_dir + "power_fill_length.pdf")
+
+fig3 = plt.figure('Z3')
+cset3 = plt.contourf(X3, Y3, Z3, levels=LEVELS)
+CB3 = plt.colorbar(
+    cset3, orientation='vertical', format=FORMAT, fraction=FRACTION,
+    ticks=TICKS 
+    )
+CB3.set_label(r'Power Flux (kW/m$^2$)')
+plt.xticks(rotation=40)
+plt.xlabel('Length (mm)')
+plt.ylabel('Current (A)')
+plt.subplots_adjust(bottom=BOTTOM, right=RIGHT)
+plt.grid()
+fig3.savefig(save_dir + "power_length_I.pdf")
+
+plt.show()
+
+
