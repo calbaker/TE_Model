75c6593957350d9caee8087eca0b6579b12fd243
Chad Baker <calbaker@utexas.edu>
2012-09-28 14:52:28 -0500

    statistical data fitting runs.  it might even run correctly.  I'll be making plots to test this.

--- Modules/exp_data.py
+++ Modules/exp_data.py
@@ -2,11 +2,14 @@
 it."""
 
 import numpy as np
+from scipy.optimize import leastsq
 
 import properties as prop
 
+
 class DataPoint(object):
     pass
+
 
 class ExpData(object):
     """Class for containing the experimental results."""
@@ -19,6 +22,7 @@ class ExpData(object):
         self.exh = prop.ideal_gas()
         self.exh.P = 101.325
         self.cool = DataPoint()
+        self.fit_params = np.ones(6)
 
     def import_data(self):
         """Imports data from csv file as a numpy record array (aka
@@ -30,7 +34,7 @@ class ExpData(object):
         self.exh.T_out = self.data['hx_exh_out_t'] + 273.15  # K
         self.exh.mdot = self.data['exh_mdot_kgmin'] / 60.  # kg/s
         self.exh.deltaP = (
-            self.data['hx_exh_delta_p_2_in_wc'] * 0.249 * 2. # kPa
+            self.data['hx_exh_delta_p_2_in_wc'] * 0.249 * 2.  # kPa
             )
 
         self.cool.T_in = (
@@ -45,7 +49,7 @@ class ExpData(object):
 
         self.exh.T_mean = 0.5 * (self.exh.T_in + self.exh.T_out)
         self.exh.delta_T = self.exh.T_in - self.exh.T_out
-        self.exh.eta = self.exh.delta_T / (self.exh.T_in - self.cool.T_in) 
+        self.exh.eta = self.exh.delta_T / (self.exh.T_in - self.cool.T_in)
 
         self.exh.c_p = np.zeros(self.exh.T_in.size)
 
@@ -58,17 +62,31 @@ class ExpData(object):
 
     def fit_Qdot_data(self):
         """Uses scipy.optimize leastsq to minimize the error of a 2nd
-        order polynomial in fitting the experimental Qdot data.  
+        order polynomial in fitting the experimental Qdot data.
         """
-        
+        self.leastsq_out = leastsq(
+            self.get_Qdot_error, x0=self.fit_params
+            )
+        self.fit_params = self.leastsq_out[0]
+        self.rep_Qdot_data(self.fit_params)
+
     def rep_Qdot_data(self, fit_params):
         """Represents experimental Qdot data using 2-dim 2nd order
         polynomial fit."""
-        
+
+        A = fit_params[0:3]
+        B = fit_params[3:]
+
+        self.exh.Qdot_fit = (
+            A[0] + A[1] * self.exh.mdot + A[2] * self.exh.mdot ** 2. +
+            B[0] + B[1] * self.exh.T_in + B[2] * self.exh.T_in ** 2.
+            )
+
     def get_Qdot_error(self, fit_params):
         """Returns error between statistical fit and experimental Qdot
         data."""
 
-        self.Qdot_fit = 
-        
-        
+        self.rep_Qdot_data(fit_params)
+        self.exh.Qdot_fit_err = self.exh.Qdot - self.exh.Qdot_fit
+
+        return self.exh.Qdot_fit_err
