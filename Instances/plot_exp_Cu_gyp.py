e4c7d51e4a470aa254087005b3be295fe53c4841
Chad Baker <calbaker@utexas.edu>
2012-11-05 12:31:53 -0600

    first commit

---
+++ Instances/plot_exp_Cu_gyp.py
@@ -0,0 +1,65 @@
+"""Plots results from heat exchanger experiments."""
+
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+# pychecker will flag this statement, but it is necessary
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open('../output/model_validation/file',"r")
+FILE = f.read()
+f.close()
+
+FILE = 'copper'
+
+npzfile = np.load(
+    '../output/model_validation/' + FILE + '.npz'
+    ) 
+Cu_deltaP = npzfile['deltaP']
+Cu_mdot = npzfile['mdot']
+Cu_T_in = npzfile['T_in']
+Cu_Qdot = npzfile['Qdot']
+Cu_Re_D = npzfile['Re_D']
+
+FILE = 'gypsum'
+
+npzfile = np.load(
+    '../output/model_validation/' + FILE + '.npz'
+    ) 
+gyp_deltaP = npzfile['deltaP']
+gyp_mdot = npzfile['mdot']
+gyp_T_in = npzfile['T_in']
+gyp_Qdot = npzfile['Qdot']
+gyp_Re_D = npzfile['Re_D']
+
+# Plot configuration
+FONTSIZE = 24
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.markersize'] = 10
+
+plt.show()
+plt.close()
+    
+plt.figure('Qdot v mdot')
+plt.plot(gyp_mdot, gyp_Qdot, label='gypsum')
+plt.plot(Cu_mdot, Cu_Qdot, label='Cu mesh')
+plt.xlabel('Exhaust Mass Flow Rate (kg/s)')
+plt.ylabel('Heat Transfer Rate (kW)')
+plt.grid()
+plt.savefig('../Plots/plot_exp/exp_Qdot_v_mdot.pdf')
+
+plt.figure('Qdot v T_in')
+plt.plot(gyp_T_in, gyp_Qdot, label='gypsum')
+plt.plot(Cu_T_in, Cu_Qdot, label='Cu mesh')
+plt.xlabel('Exhaust Mass Flow Rate (kg/s)')
+plt.ylabel('Heat Transfer Rate (kW)')
+plt.grid()
+plt.savefig('../Plots/plot_exp/exp_Qdot_v_T_in.pdf')
+
+plt.show()
